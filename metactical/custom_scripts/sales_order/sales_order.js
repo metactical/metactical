e63df13b4b6180342036ff69780da2f180a70cc2
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-03-19 14:31:50 +0300

    Temporarily returned the udate qty and rate btn in Sales Order

--- metactical/custom_scripts/sales_order/sales_order.js
+++ metactical/custom_scripts/sales_order/sales_order.js
@@ -2,10 +2,10 @@ frappe.ui.form.on('Sales Order', {
 	refresh: function(frm){
 		//Clear update qty and rate button
 		console.log(frm);
-		if(frm.doc.docstatus === 1 && frm.doc.status !== 'Closed'
+		/*if(frm.doc.docstatus === 1 && frm.doc.status !== 'Closed'
 			&& flt(frm.doc.per_delivered, 6) < 100 && flt(frm.doc.per_billed, 6) < 100) {
 			frm.clear_custom_buttons();
-		}
+		}*/
 		
 		setTimeout(() => {
 			frm.remove_custom_button("Pick List", 'Create'); 
