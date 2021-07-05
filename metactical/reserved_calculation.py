6fb336dc22a9391d5aab0106021785024b374105
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-07-05 18:36:39 +0300

    Only update if reserved qty different from current value

--- metactical/reserved_calculation.py
+++ metactical/reserved_calculation.py
@@ -2,9 +2,11 @@ import frappe
 from erpnext.stock.stock_balance import update_bin_qty, get_reserved_qty
 
 def recalculate_reserved_qty():
-	items = frappe.get_all("Bin", fields=['item_code', 'warehouse'])
+	items = frappe.get_all("Bin", fields=['item_code', 'warehouse', 'reserved_qty'])
 	for item in items:
-		update_bin_qty(item.item_code, item.warehouse, {
-			"reserved_qty": get_reserved_qty(item.item_code, item.warehouse)
-		})
+		reserved_qty = get_reserved_qty(item.item_code, item.warehouse)
+		if reserved_qty != item.reserved_qty:
+			update_bin_qty(item.item_code, item.warehouse, {
+				"reserved_qty": reserved_qty
+			})
 		
