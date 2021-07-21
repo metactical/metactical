300d345b82c9168613b506ade8f631ccf8cbc2ac
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-07-21 23:26:16 +0300

    Removed after saved alert from delivery note

--- metactical/custom_scripts/delivery_note/delivery_note.py
+++ metactical/custom_scripts/delivery_note/delivery_note.py
@@ -2,5 +2,4 @@ import frappe
 from metactical.api.shipstation import create_shipstation_orders
 
 def on_update(self, method):
-	frappe.msgprint("After save called")
 	create_shipstation_orders(self.name)
