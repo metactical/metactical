cf05a9fb9af3c5d551dca1c0d12b76460a1c012d
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-08-05 16:46:39 +0300

    Reorganized Metactical menu

--- metactical/config/metactical.py
+++ metactical/config/metactical.py
@@ -9,77 +9,83 @@ def get_data():
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
+			"label": _("Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "POS Discount Report",
+					"doctype": "Sales Invoice"
+				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Stock Reconciliation Report",
+					"doctype": "Stock Reconciliation"
+				},
+				{
+					"type": "report",
+					"name": "Item-Wise Sales Invoice Report",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+				{
+					"type": "report",
+					"name": "Sales Report RASUSA - Full V1",
+					"doctype": "Sales Invoice",
+					"is_query_report": True
+				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Payments Status",
+					"doctype": "Sales Order",
+				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Ready to Ship - Orders",
+					"doctype": "Sales Order",
+				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Pick List Status",
+					"doctype": "Sales Order",
+				},
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Sales Report - Stores",
+					"doctype": "Stock Ledger Entry",
+				},
+			]
+		},
+		{
 			"label": _("Metactical Items"),
 			"items": [
 				{
 					"type": "doctype",
 					"name": "Item Search Settings",
-					"description": _("Item Search Settings."),
-					"onboard": 1,
+					"description": _("Item Search Settings.")
 				},
 				{
 					"type": "doctype",
-					"name": "Item Search Settings Warehouse",
-					"description": _("Item Search Settings Warehouse."),
-					"onboard": 1,
+					"name": "Shipstation Settings",
+					"description": _("Shipstation Settings")
 				},
 				{
 					"type": "doctype",
 					"name": "Shipstation API Requests",
-					"description": _("Shipstation API Requests."),
-					"onboard": 1,
+					"description": _("Shipstation API Requests.")
 				},
+				{
+					"type": "page",
+					"name": "packing-slip",
+					"label": _("Packing Slip"),
+					"description": _("Packing Slip Page.")
+				}
 			]
 		},
-		{
-			"label": _("Reports"),
-			"icon": "fa fa-list",
-			"items": [
-					{
-						"type": "report",
-						"is_query_report": True,
-						"name": "POS Discount Report",
-						"doctype": "Sales Invoice",
-						"onboard": 1,
-					},
-					{
-						"type": "report",
-						"name": "Item-Wise Sales Invoice Report",
-						"doctype": "Sales Invoice",
-						"is_query_report": True,
-						"onboard": 1,
-					},
-					{
-						"type": "report",
-						"name": "Sales Report RASUSA - Full V1",
-						"doctype": "Sales Invoice",
-						"is_query_report": True,
-						
-					},
-					{
-						"type": "report",
-						"is_query_report": True,
-						"name": "Payments Status",
-						"doctype": "Sales Order",
-					},
-					{
-						"type": "report",
-						"is_query_report": True,
-						"name": "Ready to Ship - Orders",
-						"doctype": "Sales Order",
-					},
-					{
-						"type": "report",
-						"is_query_report": True,
-						"name": "Pick List Status",
-						"doctype": "Sales Order",
-					},
-					{
-						"type": "report",
-						"is_query_report": True,
-						"name": "Sales Report - Stores",
-						"doctype": "Stock Ledger Entry",
-					},
-				]
-			},
-		]
+	]
