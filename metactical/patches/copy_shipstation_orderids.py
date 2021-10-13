b99dde32e3f34a0e17aea3477c36ca5fe5df4655
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-10-13 14:31:36 +0300

    Fixed patch to load shipstation id doctype before patching

--- metactical/patches/copy_shipstation_orderids.py
+++ metactical/patches/copy_shipstation_orderids.py
@@ -2,15 +2,17 @@ import frappe
 from frappe.utils.fixtures import sync_fixtures
 
 def execute():
+	frappe.reload_doc('metactical', 'doctype', 'Shipstation Order ID')
 	sync_fixtures()
 	frappe.reload_doc('stock', 'doctype', 'Delivery Note')
 	delivery_notes = frappe.db.sql('''SELECT name FROM `tabDelivery Note` WHERE ais_shipstation_orderid IS NOT NULL''', as_dict=1)
 	shipstation_settings = frappe.db.sql('''SELECT name FROM `tabShipstation Settings` LIMIT 1''', as_dict=1)
 	for row in delivery_notes:
 		delivery_note = frappe.get_doc('Delivery Note', row.name)
-		shipstation_orderid = frappe.new_doc('Shipstation Order ID', delivery_note, 'ais_shipstation_order_ids')
-		shipstation_orderid.update({
-			"settings_id": shipstation_settings[0].name,
-			"shipstation_order_id": delivery_note.ais_shipstation_orderid
-		})
-		shipstation_orderid.save()
+		if len(delivery_note.ais_shipstation_order_ids) == 0:
+			shipstation_orderid = frappe.new_doc('Shipstation Order ID', delivery_note, 'ais_shipstation_order_ids')
+			shipstation_orderid.update({
+				"settings_id": shipstation_settings[0].name,
+				"shipstation_order_id": delivery_note.ais_shipstation_orderid
+			})
+			shipstation_orderid.save()
