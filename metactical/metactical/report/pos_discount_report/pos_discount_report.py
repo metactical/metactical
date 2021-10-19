3c3092caad32607dc2432535cf8dac35ce640299
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-10-19 11:49:18 +0300

    Removed shipping items from appearing in report

--- metactical/metactical/report/pos_discount_report/pos_discount_report.py
+++ metactical/metactical/report/pos_discount_report/pos_discount_report.py
@@ -16,28 +16,29 @@ def execute(filters=None):
 
 	item_sales = get_data(filters)
 	for d in item_sales:
-		if d.get('price_list_rate') > 0:
-			price_list_rate = frappe.db.get_value("Item Price", 
-				{"price_list": d.get('selling_price_list'), "selling": 1, "item_code": d.get('item_code')}, "price_list_rate")
-			#rate_discount = (d.get('price_list_rate') - d.get('rate'))/d.get('price_list_rate')
-			if price_list_rate is not None:
-				rate_discount = (price_list_rate - d.get('rate'))/d.get('price_list_rate')
-				if rate_discount >= 0.15:
-					row = {}
-					row['si_date'] = d.posting_date
-					row['warehouse'] = d.warehouse
-					row['si_name'] = d.name
-					row['ifw_retailskusuffix'] = d.ifw_retailskusuffix
-					row['item_code'] = d.item_code
-					row['item_name'] = d.item_name
-					row['qty'] = d.qty
-					row['rate'] = d.rate
-					row['price_list_rate'] = price_list_rate
-					row['discount_percentage'] = rate_discount * 100
-					row['uom'] = d.uom
-					row['ifw_location'] = d.ifw_location
+		if d.item_code != "9999-tempt" and d.variant_of != "9999-tempt": #remove shipping items from list
+			if d.get('price_list_rate') > 0:
+				price_list_rate = frappe.db.get_value("Item Price", 
+					{"price_list": d.get('selling_price_list'), "selling": 1, "item_code": d.get('item_code')}, "price_list_rate")
+				#rate_discount = (d.get('price_list_rate') - d.get('rate'))/d.get('price_list_rate')
+				if price_list_rate is not None:
+					rate_discount = (price_list_rate - d.get('rate'))/d.get('price_list_rate')
+					if rate_discount >= 0.15:
+						row = {}
+						row['si_date'] = d.posting_date
+						row['warehouse'] = d.warehouse
+						row['si_name'] = d.name
+						row['ifw_retailskusuffix'] = d.ifw_retailskusuffix
+						row['item_code'] = d.item_code
+						row['item_name'] = d.item_name
+						row['qty'] = d.qty
+						row['rate'] = d.rate
+						row['price_list_rate'] = price_list_rate
+						row['discount_percentage'] = rate_discount * 100
+						row['uom'] = d.uom
+						row['ifw_location'] = d.ifw_location
 
-					data.append(row)
+						data.append(row)
 
 	return columns, data
 
@@ -96,7 +97,7 @@ def get_data(filters):
 
 	data = frappe.db.sql("""select c.item_code, c.item_name, c.qty, c.price_list_rate, c.rate, c.discount_percentage,
 		c.uom, c.ifw_retailskusuffix, c.ifw_location, c.warehouse,
-		p.name, p.posting_date, p.selling_price_list
+		p.name, p.posting_date, p.selling_price_list, i.variant_of
 		from `tabSales Invoice Item` c inner join `tabSales Invoice` p on p.name = c.parent
 		inner join `tabItem` i on c.item_code = i.name 
 		where p.docstatus = 1 and p.posting_date BETWEEN %(from_date)s AND %(to_date)s
