2786df4e3daff7f0a7941eb419d39d69746e543b
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2021-11-10 22:17:30 +0300

    Added custom purchase order print format version 6

--- metactical/custom_scripts/purchase_order/purchase_order.py
+++ metactical/custom_scripts/purchase_order/purchase_order.py
@@ -22,3 +22,20 @@ def shipping_address_query(doctype, txt, searchfield, start, page_len, filters):
 			`tabAddress`.is_your_company_address = 1)''',
 			{'link_name': link_name, 'link_doctype': link_doctype, 'company': company}
 			)
+			
+@frappe.whitelist()
+def get_po_items(docname):
+	items = []
+	added_items = []
+	doc = frappe.get_doc('Purchase Order', docname)
+	for item in doc.items:
+		if item.item_code not in added_items:
+			items.append(item)
+			added_items.append(item.item_code)
+		else:
+			for i in items:
+				if i.item_code == item.item_code:
+					i.update({
+						'qty': i.qty + item.qty
+					})
+	return items
--- metactical/hooks.py
+++ metactical/hooks.py
@@ -158,4 +158,11 @@ override_whitelisted_methods = {
 #Fixtures
 fixtures = ["Custom Field", "Property Setter", "Custom Script"]
 
+#For using in print format
+jenv = {
+	"methods": [
+		"get_po_items:metactical.custom_scripts.purchase_order.purchase_order.get_po_items"
+	]
+}
+
 
