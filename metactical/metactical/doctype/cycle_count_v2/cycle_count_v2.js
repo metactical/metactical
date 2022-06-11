89af8b8a8ee87a5f12fa9b3ce5e4ae305111fc2a
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2022-06-11 18:03:39 +0300

    Added cycle count v2

---
+++ metactical/metactical/doctype/cycle_count_v2/cycle_count_v2.js
@@ -0,0 +1,125 @@
+// Copyright (c) 2022, Techlift Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Cycle Count V2', {
+	onload: function(frm){
+		frm.set_query('warehouse', function(){
+			return {
+				query: "metactical.metactical.doctype.cycle_count_v2.cycle_count_v2.get_permitted_warehouses",
+				filters: {'user': frappe.session.user}
+			};
+		})
+	},
+	
+	get_items: function(frm){
+		frappe.call({
+			method: "metactical.metactical.doctype.cycle_count_v2.cycle_count_v2.get_items",
+			args: {
+				'warehouse': frm.doc.warehouse,
+				'ifw_location': frm.doc.ifw_location
+			},
+			freeze: true,
+			callback: function(ret){
+				console.log(ret);
+				if(typeof ret.message != undefined){
+					frm.doc.items = [];
+					for(let row in ret.message){
+						var child = cur_frm.add_child("items");
+						child.item_code = ret.message[row].item_code;
+						child.expected_qty = ret.message[row].actual_qty;
+						child.valuation_rate = ret.message[row].valuation_rate;
+					}
+					cur_frm.refresh_fields('items');
+				}
+			}
+		})
+	},
+	
+	scan_barcode: function(frm) {
+		let scan_barcode_field = frm.fields_dict["scan_barcode"];
+
+		let show_description = function(idx, exist = null) {
+			if (exist) {
+				scan_barcode_field.set_new_description(__('Row #{0}: Qty increased by 1', [idx]));
+			} else {
+				scan_barcode_field.set_new_description(__('Row #{0}: Item added', [idx]));
+			}
+		}
+
+		if(frm.doc.scan_barcode) {
+			frappe.call({
+				method: "erpnext.selling.page.point_of_sale.point_of_sale.search_for_serial_or_batch_or_barcode_number",
+				args: { search_value: frm.doc.scan_barcode }
+			}).then(r => {
+				const data = r && r.message;
+				if (!data || Object.keys(data).length === 0) {
+					scan_barcode_field.set_new_description(__('Cannot find Item with this barcode'));
+					return;
+				}
+
+				let cur_grid = cur_frm.fields_dict.items.grid;
+
+				let row_to_modify = null;
+				const existing_item_row = cur_frm.doc.items.find(d => d.item_code === data.item_code);
+				const blank_item_row = cur_frm.doc.items.find(d => !d.item_code);
+
+				if (existing_item_row) {
+					row_to_modify = existing_item_row;
+				} else if (blank_item_row) {
+					row_to_modify = blank_item_row;
+				}
+
+				if (!row_to_modify) {
+					// add new row
+					//row_to_modify = frappe.model.add_child(cur_frm.doc, cur_grid.doctype, 'items');
+					frappe.throw('Error: Item not in location')
+				}
+
+				//show_description(row_to_modify.idx, row_to_modify.item_code);
+
+				cur_frm.from_barcode = true;
+				frappe.model.set_value(row_to_modify.doctype, row_to_modify.name, {
+					item_code: data.item_code,
+					qty: (row_to_modify.qty || 0) + 1
+				});
+
+				/*['serial_no', 'batch_no', 'barcode'].forEach(field => {
+					if (data[field] && frappe.meta.has_field(row_to_modify.doctype, field)) {
+
+						let value = (row_to_modify[field] && field === "serial_no")
+							? row_to_modify[field] + '\n' + data[field] : data[field];
+
+						frappe.model.set_value(row_to_modify.doctype,
+							row_to_modify.name, field, value);
+					}
+				});*/
+
+				scan_barcode_field.set_value('');
+				refresh_field("items");
+			});
+		}
+		return false;
+	},
+});
+
+/*frappe.ui.form.on('Cycle Count V2 Item', {
+	item_code: function(frm, cdt, cdn){
+		if(frm.doc.warehouse && locals[cdt][cdn].item_code){
+			var item_code = locals[cdt][cdn].item_code;
+			var warehouse = frm.doc.warehouse;
+			frappe.call({
+				method: "metactical.metactical.doctype.cycle_count_v2.cycle_count_v2.get_expected_qty",
+				args: {"item_code": item_code, "warehouse": warehouse},
+				freeze: true,
+				callback: function(ret){
+					console.log(ret);
+					if(typeof ret.message != undefined){
+						frappe.model.set_value(cdt, cdn, "expected_qty", ret.message.actual_qty);
+						frappe.model.set_value(cdt, cdn, "valuation_rate", ret.message.valuation_rate);
+					}
+				}
+			});
+		}
+	}
+});*/
+
---
+++ metactical/metactical/doctype/cycle_count_v2/cycle_count_v2.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2022, Techlift Technologies and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class CycleCountV2(Document):
+	def on_submit(self):
+		doc = frappe.new_doc("Stock Reconciliation");
+		doc.update({
+			"purpose": "Stock Reconciliation",
+			"ais_cycle_count_v2": self.name
+		})
+		for row in self.items:
+			doc.append("items", {
+				"item_code": row.item_code,
+				"warehouse": self.warehouse,
+				"qty": row.qty,
+				"valuation_rate": row.valuation_rate
+			})
+		doc.submit()
+		
+@frappe.whitelist()
+def get_expected_qty(item_code, warehouse):
+	expected = frappe.db.sql('''SELECT actual_qty, valuation_rate FROM `tabBin` 
+								WHERE item_code = %(item_code)s AND warehouse = %(warehouse)s''', 
+								{"item_code": item_code, "warehouse": warehouse}, as_dict=1)
+	if expected and len(expected) > 0:
+		ret = {"actual_qty": expected[0].actual_qty, "valuation_rate": expected[0].valuation_rate}
+		return ret
+	else:
+		return {"actual_qty": 0, "valuation_rate": 0}
+		
+@frappe.whitelist()
+def get_permitted_warehouses(doctype, txt, searchfield, start, page_len, filters):
+	user = filters.get("user")
+	warehouses = []
+	if user:
+		setting_exists = frappe.db.get_value("Stock Entry User Permissions", filters={"user": user})
+		if setting_exists:
+			warehouses = frappe.db.sql("""SELECT warehouse FROM `tabUser Permitted Warehouse` 
+							WHERE warehouse LIKE %(txt)s AND parent= %(parent)s
+							AND parentfield='cycle_count_warehouse'""", 
+							{
+								'txt': "%%%s%%" % txt,
+								'parent': setting_exists
+							})
+		else:
+			#Retrun all warehouses
+			warehouses = frappe.db.sql("""SELECT name FROM `tabWarehouse` WHERE is_group=0 AND disabled=0 AND name LIKE %(txt)s""", {'txt': "%%%s%%" % txt})
+	return warehouses
+	
+@frappe.whitelist()
+def get_items(warehouse, ifw_location):
+	items = frappe.db.sql("""SELECT 
+								item.item_code, bin.actual_qty, bin.valuation_rate
+							FROM 
+								`tabItem` AS item
+							LEFT JOIN
+								`tabBin` AS bin ON bin.item_code = item.item_code AND warehouse = '{0}'
+							WHERE
+								item.ifw_location LIKE '{1}'
+							""".format(warehouse, '%' + ifw_location + '%'), as_dict=1)
+	return items
