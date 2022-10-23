100f90a3a7b755c0e67538d02f8df470206a4bd3
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2022-10-23 23:47:13 +0300

    Added default price list to sales report

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -197,7 +197,13 @@ override_whitelisted_methods = {
 #}
 
 #Fixtures
-fixtures = ["Custom Field", "Property Setter", "DocType Link", "List View Settings"]
+fixtures = [{
+    "dt": "Custom Field",
+    "filters": [["name", "in", [
+        'Stock Settings-ais_default_price_list',
+        'Stock Settings-ais_sales_report_settings'
+    ]]]
+}]
 
 #For using in print format
 jenv = {
