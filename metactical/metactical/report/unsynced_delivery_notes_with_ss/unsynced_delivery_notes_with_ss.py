c0d71834458da48d0d87b3d1b9af63e179ba1077
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2022-11-03 20:26:49 +0300

    Added unsynced delivery notes with SS report

---
+++ metactical/metactical/report/unsynced_delivery_notes_with_ss/unsynced_delivery_notes_with_ss.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2022, Techlift Technologies and contributors
+# For license information, please see license.txt
+
+import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	columns = get_columns(filters)
+	data = get_data(filters)
+	return columns, data
+	
+def get_data(filters):
+	query = frappe.db.sql("""SELECT
+										delivery_note.name AS reference, delivery_note.posting_date,
+										'Not Synced With SS' AS error
+									FROM
+										`tabDelivery Note` AS delivery_note
+									WHERE
+										(SELECT name FROM `tabShipstation Order ID` 
+											AS order_id WHERE order_id.parent = delivery_note.name
+											LIMIT 1) > 0
+										AND ((delivery_note.shipping_address_name is not NULL
+										AND delivery_note.shipping_address_name != "") OR 
+										(delivery_note.customer_address is not NULL AND 
+										delivery_note.customer_address != '')) AND delivery_note.docstatus = 0
+									""", as_dict=1)
+	query2 = frappe.db.sql("""SELECT
+										delivery_note.name AS reference, delivery_note.posting_date,
+										'No Address' AS error
+									FROM
+										`tabDelivery Note` AS delivery_note
+									WHERE
+										(SELECT name FROM `tabShipstation Order ID` 
+											AS order_id WHERE order_id.parent = delivery_note.name
+											LIMIT 1) > 0
+										AND ((delivery_note.shipping_address_name is NULL
+										OR delivery_note.shipping_address_name != "") AND
+										(delivery_note.customer_address is NULL OR 
+										delivery_note.customer_address != ''))  AND delivery_note.docstatus = 0
+									""", as_dict=1)
+	data = query + query2
+	return data
+	
+def get_columns(filters):
+	columns = [
+		{
+			"fieldname": "reference",
+			"fieldtype": "Link",
+			"label": "Delivery Note",
+			"options": "Delivery Note",
+			"width": 200
+		},
+		{
+			"fieldname": "posting_date",
+			"fieldtype": "Date",
+			"label": "Posting Date",
+			"width": 200
+		},
+		{
+			"fieldname": "error",
+			"fieldtype": "Data",
+			"label": "Error",
+			"width": 200
+		}
+	]
+	return columns	
