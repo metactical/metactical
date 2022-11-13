de35e2173d3e2fe0917359f0d271b91e88e37c73
Aisenyi Malisa <malisa.aisenyi@gmail.com>
2022-11-13 23:47:28 +0300

    Added supplier status report

---
+++ metactical/metactical/report/supplier_list/supplier_list.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Techlift Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Supplier List"] = {
+	"filters": [
+
+	]
+};
