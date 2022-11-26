609da0d5e1acebd78e79584969e01506befd83ec
Aisenyi <malisa.aisenyi@gmail.com>
2022-11-26 20:08:36 +0300

    Changed supplier list report to show all open pos

--- metactical/metactical/report/supplier_list/supplier_list.py
+++ metactical/metactical/report/supplier_list/supplier_list.py
@@ -6,7 +6,7 @@ import frappe
 def execute(filters=None):
 	columns, data = [], []
 	columns = get_columns(filters)
-	data = get_data(filters)
+	data = get_pos(filters)
 	return columns, data
 
 def get_data(filters):
@@ -42,6 +42,22 @@ def get_last_po(supplier):
 		return data[0]
 	else:
 		return frappe._dict()
+		
+def get_pos(filters):
+	data = frappe.db.sql("""SELECT
+								po.supplier, po.name AS po_no, po.transaction_date AS po_date,
+								pr.name AS recent_pr, pr.status AS pr_status, po.eta_date AS eta,
+								po.carrier_used, po.tracking_id, po.drop_ship_notes AS notes
+							FROM
+								`tabPurchase Order` AS po
+							LEFT JOIN
+								`tabPurchase Receipt` AS pr ON pr.purchase_order = po.name
+							WHERE
+								po.docstatus = 1 AND po.status <> 'Completed'
+							ORDER BY
+								po.supplier, po.transaction_date DESC
+							""", as_dict=1)
+	return data
 
 def get_columns(filters):
 	columns = [
@@ -53,7 +69,7 @@ def get_columns(filters):
 			"width": 150
 		},
 		{
-			"fieldname": "recent_po",
+			"fieldname": "po_no",
 			"fieldtype": "Link",
 			"label": "PO Number",
 			"options": "Purchase Order",
@@ -62,7 +78,7 @@ def get_columns(filters):
 		{
 			"fieldname": "po_date",
 			"fieldtype": "Date",
-			"label": "Most Recent PO Date",
+			"label": "PO Date",
 			"width": 150
 		},
 		{
