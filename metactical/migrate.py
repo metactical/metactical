ab98261f39b0f0977c4947a97558de30b3c98de1
Aisenyi <malisa.aisenyi@gmail.com>
2022-12-20 21:11:09 +0300

    Made PO name property setter to be run after every migrate

--- metactical/migrate.py
+++ metactical/migrate.py
@@ -4,17 +4,21 @@ def after_migrate():
 	#Reset naming series in PO based on site name
 	exists = frappe.db.exists('Property Setter', {'doctype_or_field': 'DocField', 'doc_type': 'Purchase Order',
 					'field_name': 'naming_series', 'property': 'options'})
-	if not exists:
-		site = frappe.local.site
-		value = '.YYYY.-'
-		if site == 'usa.metactical.com':
-			value = '3002-'
-		doc = frappe.new_doc('Property Setter')
-		doc.update({
-			'doctype_or_field': 'DocField', 
-			'doc_type': 'Purchase Order',
-			'field_name': 'naming_series', 
-			'property': 'options',
-			'value': value
-		})
-		doc.insert()
+	if exists:
+		doc = frappe.db.get_value('Property Setter', {'doctype_or_field': 'DocField', 'doc_type': 'Purchase Order',
+					'field_name': 'naming_series', 'property': 'options'}, 'name')
+		frappe.db.delete('Property Setter', doc)
+		
+	site = frappe.local.site
+	value = '.YYYY.-'
+	if site == 'usa.metactical.com':
+		value = '3002-'
+	doc = frappe.new_doc('Property Setter')
+	doc.update({
+		'doctype_or_field': 'DocField', 
+		'doc_type': 'Purchase Order',
+		'field_name': 'naming_series', 
+		'property': 'options',
+		'value': value
+	})
+	doc.insert()
