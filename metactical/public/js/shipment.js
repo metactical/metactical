9dd87d6ad7db9b52dd383b0033c38d355527f33c
Mainul <mainulkhan94@gmail.com>
2023-01-21 15:55:13 +0600

    feat: Created Multiple Shipments for multiple parcels

--- metactical/public/js/shipment.js
+++ metactical/public/js/shipment.js
@@ -31,60 +31,37 @@ const ShipmentController = frappe.ui.form.Controller.extend({
                 minimizable: true,
                 primary_action: () => {
                     this.rateDialog.disable_primary_action()
-                    let carrier_service = $(this.rateDialog.body).find('[name="carrier_service"]:checked').val()
+                    let carrier_service = {}
+                    $(this.rateDialog.body).find('[name^="carrier_service_"]:checked').each(function(){
+                        carrier_service[$(this).closest('table').attr('data-row-name')] = $(this).val()
+                    })
+                    if ($.isEmptyObject(carrier_service)) {
+                        frappe.msgprint(__("Please select min one."))
+                        return
+                    }
                     frappe.xcall("metactical.utils.shipping.shipping.create_shipping", {
                         name: this.frm.docname,
                         provider: this.frm.doc.service_provider,
-                        carrier_service: this.rate_dict[carrier_service]
+                        carrier_service: carrier_service
                     }).then(r => {
                         this.rateDialog.enable_primary_action()
                         this.rateDialog.hide()
                         this.frm.reload_doc()
                     })
                 },
-                primary_action_label: __("Create Shipmnet")
+                primary_action_label: __(`Create Shipmnet<small>(s)</small>`)
             })
         }
         this.rateDialog.enable_primary_action()
-        let [rows, last_id] = this.get_html()
-        this.rateDialog.$body.html(`
-        <table class="table table-bordered">
-            <tr>
-                <th></th>
-                <th>${__("Service")}</th>
-                <th>${__("Base Price")}</th>
-                <th>${__("Total")}</th>
-                <th>${__("Guaranteed Delivery")}</th>
-                <th>${__("Expected Transit Time")}</th>
-                <th>${__("Expected Delivery Date")}</th>
-            </tr>
-            ${rows}
-        </table>`)
-        this.rateDialog.$body.find(`[value="${last_id}"]`).prop('checked', true)
-        this.rateDialog.show()
-    },
-    get_html: function () {
-        let html = ''
-        let last_rate=0;
-        let last_id;
-        this.rate_dict = {}
-        this.rates.forEach(row => {
-            if (last_rate > flt(row.shipment_amount) || last_rate===0) {
-                last_id=row.carrier_service
-                last_rate=flt(row.shipment_amount)
+        this.rateDialog.$body.html(frappe.render_template('shipment_rate', this.rates))
+        this.rateDialog.$body.find(`select[name="carrier_service"]`).on('click', ()=>{
+            let val = this.rateDialog.$body.find(`select[name="carrier_service"]`).val()
+            if (!val) {
+                return
             }
-            this.rate_dict[row.carrier_service] = row
-            html += `<tr>&nbsp;
-                        <td><input type="radio" name="carrier_service" value="${row.carrier_service}"></td>
-                        <td>${row.service_name}</td>
-                        <td>${row.base}</td>
-                        <td>${row.shipment_amount}</td>
-                        <td>${row.guaranteed_delivery ? "Yes" : "No"}</td>
-                        <td>${row.expected_transit_time}</td>
-                        <td>${row.expected_delivery_date}</td>
-            </tr>`
+            this.rateDialog.$body.find(`input[value="${val}"]`).prop('checked', true)
         })
-        return [html, last_id]
+        this.rateDialog.show()
     }
 })
 
--- metactical/utils/shipping/canada_post.py
+++ metactical/utils/shipping/canada_post.py
@@ -40,8 +40,8 @@ class CanadaPost():
 
     def get_context(self, name, context=None):
         doc = frappe.get_doc('Shipment', name)
-        if len(doc.shipment_parcel) !=1:
-            frappe.throw(_("Should be only one Shiopment Parcel row"))
+        if not doc.shipment_parcel:
+            frappe.throw(_("Should be min one Shiopment Parcel row"))
         if context:
             doc.update(context)
         doc.shipment_type = self.get_shipment_type(doc.shipment_type)
@@ -53,8 +53,9 @@ class CanadaPost():
             'User', doc.pickup_contact_person).as_dict()
         delivery_contact_doc = frappe.get_doc(
             'Contact', doc.delivery_contact_name).as_dict()
-        delivery_note = frappe.get_doc('Delivery Note', doc.shipment_delivery_note[-1].delivery_note).as_dict()
-        return {
+        delivery_note = frappe.get_doc(
+            'Delivery Note', doc.shipment_delivery_note[-1].delivery_note).as_dict()
+        return frappe._dict({
             "doc": doc.as_dict(),
             "settings": self.settings.as_dict(),
             "delivery_address_doc": delivery_address_doc,
@@ -62,8 +63,8 @@ class CanadaPost():
             "delivery_contact_doc": delivery_contact_doc,
             "pickup_contact_person_doc": pickup_person_doc,
             "delivery_note": delivery_note,
-        }
-    
+        })
+
     def get_shipment_type(self, s_type):
         return {
             "Document": "DOC",
@@ -74,69 +75,107 @@ class CanadaPost():
         }[s_type]
 
     def get_rate(self, name, context=None):
-        body = frappe.render_template(
-            "metactical/utils/shipping/templates/canada_post/request/get_rate.xml", self.get_context(name, context))
-        response = self.get_response("/rs/ship/price", body, {'Accept': 'application/vnd.cpc.ship.rate-v4+xml',
-                                                              'Content-Type': 'application/vnd.cpc.ship.rate-v4+xml'})
         res = []
-        if response and response['price-quotes'] and response['price-quotes']['price-quote']:
-            for pq in response['price-quotes']['price-quote']:
+        options = {}
+        context = self.get_context(name, context)        
+        exists = self.existing_shipments(context.doc)
+        for parcel in context.doc.shipment_parcel:
+            if (parcel.count - exists.get(parcel.name, 0)) <1:
+                continue
+            context.parcel = parcel
+            body = frappe.render_template(
+                "metactical/utils/shipping/templates/canada_post/request/get_rate.xml", context)
+            response = self.get_response("/rs/ship/price", body, {'Accept': 'application/vnd.cpc.ship.rate-v4+xml',
+                                                                  'Content-Type': 'application/vnd.cpc.ship.rate-v4+xml'})
+            items = []
+            if response and response['price-quotes'] and response['price-quotes']['price-quote']:
+                for pq in response['price-quotes']['price-quote']:
+                    options[pq['service-code']] = pq['service-name']
+                    items.append({
+                        'carrier_service': pq['service-code'],
+                        'service_name': pq['service-name'],
+                        'base': pq['price-details']['base'],
+                        'shipment_amount': pq['price-details']['due'],
+                        'guaranteed_delivery': pq['service-standard']['guaranteed-delivery'],
+                        'expected_transit_time': pq['service-standard']['expected-transit-time'],
+                        'expected_delivery_date': pq['service-standard']['expected-delivery-date'],
+                    })
+            if items:
                 res.append({
-                    'carrier_service': pq['service-code'],
-                    'service_name': pq['service-name'],
-                    'base': pq['price-details']['base'],
-                    'shipment_amount': pq['price-details']['due'],
-                    'guaranteed_delivery': pq['service-standard']['guaranteed-delivery'],
-                    'expected_transit_time': pq['service-standard']['expected-transit-time'],
-                    'expected_delivery_date': pq['service-standard']['expected-delivery-date'],
+                    'name': parcel.name,
+                    'idx': parcel.idx,
+                    'count': parcel.count,
+                    'items': items,
                 })
-        return res
+        return {'data': res, 'options': [{'key':k, 'val':v} for k,v in options.items()]}
 
-    def create_shipping(self, name, context=None):
-        if context and isinstance(context, string_types) and context.startswith('{'):
-            context = ast.literal_eval(context)
-        body = frappe.render_template(
-            "metactical/utils/shipping/templates/canada_post/request/create_shipment.xml", self.get_context(name, context))
-        response = self.get_response(
-            f"/rs/{self.settings.customer_number}/{self.settings.customer_number}/shipment", body)
-        doc = frappe.get_doc('Shipment', name)
-        if context:
-            doc.update(context)
-        doc.db_set('shipment_id', response['shipment-info']['shipment-id'])
-        doc.db_set('awb_number', response['shipment-info']['tracking-pin'])
-        doc.set('carrier', 'Canada Post')
-        doc.set('carrier_status', response['shipment-info']['shipment-status'])
-        doc.set('service_provider', 'Canada Post')
-        # Download Label
-        if self.settings.required_transmit_shipment:
-            self.get_make_transmit_shipment(name)
-        for link in response['shipment-info']['links']['link']:
-            if link['@rel'] == "self":
-                doc.set('tracking_url', link['@href'])
-            elif link['@rel'] == "price":
-                res = self.get_response(link['@href'], None, {'Accept': link['@media-type'],
-                                                                'Content-Type': link['@media-type']}, method='GET')
-                if res:
-                    doc.set('shipment_amount',
-                            res['shipment-price']['due-amount'])
-            elif link['@rel'] == "label":
-                res = self.get_response(link['@href'], None, {'Accept': link['@media-type'],
-                                                                'Content-Type': link['@media-type']}, return_request=True, method='GET')
-                self.write_file(doc, res, f"{link['@rel']}_{name}")
+    def create_shipping(self, name, carrier_service):
+        if carrier_service is None:
+            frappe.throw(_("Service Code Required. please select service"))
+        if isinstance(carrier_service, string_types) and carrier_service.startswith('{'):
+            carrier_service = ast.literal_eval(carrier_service)
+        
+        doc=frappe.get_doc('Shipment', name)
+        context = self.get_context(name)
+        exists = self.existing_shipments(context.doc)
+        for parcel in context.doc.shipment_parcel:
+            context.parcel = parcel
+            context.parcel.carrier_service = carrier_service.get(parcel.name)
+            for c in range(parcel.count - exists.get(parcel.name, 0)):
+                body = frappe.render_template(
+                    "metactical/utils/shipping/templates/canada_post/request/create_shipment.xml", context)
+                response = self.get_response(
+                    f"/rs/{self.settings.customer_number}/{self.settings.customer_number}/shipment", body)
+                row = doc.append('shipments', {
+                    'shipment_id':response['shipment-info']['shipment-id'],
+                    'awb_number': response['shipment-info']['tracking-pin'],
+                    'service_provider': 'Canada Post',
+                    'carrier_service': context.parcel.carrier_service,
+                    'tracking_status': '',
+                    'carrier_status': response['shipment-info']['shipment-status'],
+                    'row_id': parcel.name
+                })
+                # Download Label
+                # if self.settings.required_transmit_shipment:
+                #     self.get_make_transmit_shipment(name)
+                for link in response['shipment-info']['links']['link']:
+                    rel = 'tracking' if link['@rel'] == "self" else link['@rel']
+                    row.set(f'{rel}_url', f'''<link rel="self" href="{link['@href']}" media-type="{link['@media-type']}"></link>''')
+                    # if link['@rel'] == "self":
+                    #     doc.set('tracking_url', link['@href'])
+                    # elif link['@rel'] == "price":
+                    #     res = self.get_response(link['@href'], None, {'Accept': link['@media-type'],
+                    #                                                 'Content-Type': link['@media-type']}, method='GET')
+                    #     if res:
+                    #         doc.set('shipment_amount',
+                    #                 res['shipment-price']['due-amount'])
+                    # elif link['@rel'] == "label":
+                    #     res = self.get_response(link['@href'], None, {'Accept': link['@media-type'],
+                    #                                                 'Content-Type': link['@media-type']}, return_request=True, method='GET')
+                    #     self.write_file(doc, res, f"{link['@rel']}_{name}")
+                row.db_insert()
 
         doc.save()
         return doc.as_dict()
+    
+    def existing_shipments(self, doc):
+        shipments = frappe._dict()
+        for d in doc.get('shipments', []):
+            if not shipments.get(d.row_id):
+                shipments[d.row_id] = 0
+            shipments[d.row_id] = shipments[d.row_id]+1
+        return shipments
 
     def get_make_transmit_shipment(self, name):
         context = self.get_context(name)
         body = frappe.render_template(
             "metactical/utils/shipping/templates/canada_post/request/transmit_shipment.xml", context)
         response = self.get_response(
-            f"/rs/{self.settings.customer_number}/{self.settings.customer_number}/manifest", body, headers={'Accept':'application/vnd.cpc.manifest-v8+xml', 'Content-Type':'application/vnd.cpc.manifest-v8+xml'})
+            f"/rs/{self.settings.customer_number}/{self.settings.customer_number}/manifest", body, headers={'Accept': 'application/vnd.cpc.manifest-v8+xml', 'Content-Type': 'application/vnd.cpc.manifest-v8+xml'})
         po_numbers = []
         if response:
             res = self.get_response(response['manifests']['link']['@href'], None, {'Accept': response['manifests']['link']['@media-type'],
-                                                                  'Content-Type': response['manifests']['link']['@media-type']}, method='GET')
+                                                                                   'Content-Type': response['manifests']['link']['@media-type']}, method='GET')
             if res:
                 po_numbers.append(res['manifest']['po-number'])
                 # for l in res['manifest']['links']['link']:
