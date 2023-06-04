ae2a70d4d303bf608fc985f4f76c9e245eb610d7
Aisenyi <malisa.aisenyi@gmail.com>
2023-06-04 20:53:04 +0300

    chore: Make scan to add in Packing Page permission based

--- metactical/metactical/page/packing_page/packing_page.py
+++ metactical/metactical/page/packing_page/packing_page.py
@@ -3,3 +3,11 @@ import frappe
 @frappe.whitelist()
 def get_delivery_from_tote(tote):
 	return frappe.db.get_value('Picklist Tote', tote, 'current_delivery_note')
+	
+@frappe.whitelist()
+def check_to_add_permission():
+	has_permission = frappe.db.exists('Packing Allowed User', {"user": frappe.session.user})
+	if not has_permission:
+		return False
+	else:
+		return True
