3074f29c3bed8ac4291c525508197ffc698d87fa
Aisenyi <malisa.aisenyi@gmail.com>
2023-07-14 11:41:09 +0300

    chore: Added sales report by location

--- metactical/metactical/report/sales_report_by_location/sales_report_by_location.js
+++ metactical/metactical/report/sales_report_by_location/sales_report_by_location.js
@@ -2,28 +2,31 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Sales Report By Location"] = {
+frappe.query_reports["Sales Report by Location"] = {
 	"filters": [
 		{
-			"fieldname":"supplier",
-			"label": __("Supplier"),
-			"fieldtype": "MultiSelectList",
-			"options": "Supplier",
-			get_data: function(txt) {
-				// if (!frappe.query_report.filters) return;
-
-				// let party_type = frappe.query_report.get_filter_value('party_type');
-				// if (!party_type) return;
-
-				return frappe.db.get_link_options("Supplier", txt);
-			},
+			fieldtype: "Date",
+			fieldname: "from_date",
+			label: "From Date",
+			reqd: 1
 		},
 		{
-			"fieldname":"limit",
-			"label": __("Limit"),
-			"fieldtype": "Select",
-			"options": ["20", "500", "1000", "5000", "10000", "All"],
-			"default": "20"
+			fieldtype: "Date",
+			fieldname: "to_date",
+			label: "To Date",
+			reqd: 1
 		},
+		{
+			fieldtype: "Link",
+			fieldname: "warehouse",
+			options: "Warehouse",
+			label: "Location"
+		},
+		{
+			fieldtype: "Link",
+			fieldname: "supplier",
+			options: "Supplier",
+			label: "Supplier"
+		}
 	]
 };
--- metactical/metactical/report/sales_report_by_location/sales_report_by_location.py
+++ metactical/metactical/report/sales_report_by_location/sales_report_by_location.py
@@ -6,76 +6,61 @@ import frappe
 def execute(filters=None):
     data = []
     columns = [
-        {"label": "Sales Order", "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order"},
-        {"label": "Transaction Date", "fieldname": "transaction_date", "fieldtype": "Date"},
-        {"label": "Customer", "fieldname": "customer", "fieldtype": "Link", "options": "Customer"},
-        {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Link", "options": "Item"},
-        {"label": "Quantity", "fieldname": "quantity", "fieldtype": "Float"},
-        {"label": "Rate", "fieldname": "rate", "fieldtype": "Currency"},
-        {"label": "Amount", "fieldname": "amount", "fieldtype": "Currency"},
         {"label": "Brand", "fieldname": "brand", "fieldtype": "Link", "options": "Brand"},
         {"label": "Supplier", "fieldname": "supplier", "fieldtype": "Link", "options": "Supplier"},
         {"label": "Retail SKU", "fieldname": "retail_sku", "fieldtype": "Data"},
+        {"label": "Location", "fieldname": "location", "fieldtype": "Link", "options": "Warehouse"},
         {"label": "QOH for Location", "fieldname": "qoh_location", "fieldtype": "Float"},
         {"label": "TQOH for All Locations", "fieldname": "tqoh_all_locations", "fieldtype": "Float"},
         {"label": "Retail Price", "fieldname": "retail_price", "fieldtype": "Currency"},
         {"label": "Cost", "fieldname": "cost", "fieldtype": "Currency"},
-        {"label": "Qty Sold", "fieldname": "qty_sold", "fieldtype": "Float"},
+        {"label": "Qty Sold", "fieldname": "qty_sold", "fieldtype": "Float"}
     ]
 
-    warehouse = filters.get("warehouse") 
+    warehouse = filters.get("warehouse")
+    supplier = filters.get("supplier")
     from_date = filters.get("from_date")
     to_date = filters.get("to_date")
+    
+    where_extra = ""
+    if warehouse and warehouse != "":
+        where_extra += " AND sii.warehouse = '{warehouse}'".format(warehouse = warehouse)
+    
+    if supplier and supplier != "":
+        where_extra += " AND defaults.default_supplier = '{supplier}'".format(supplier = supplier)
 
     # Query sales orders and related information
-    sales_orders = frappe.db.sql("""
+    data = frappe.db.sql("""
         SELECT
-            so.name AS sales_order,
-            so.transaction_date AS transaction_date,
-            so.customer AS customer,
-            si.item_code AS item_code,
-            si.qty AS quantity,
-            si.rate AS rate,
-            si.amount AS amount,
+            sii.item_code AS item_code,
             i.brand AS brand,
-            i.supplier AS supplier,
-            i.retail_sku AS retail_sku,
+            defaults.default_supplier AS supplier,
+            i.ifw_retailskusuffix AS retail_sku,
+            sii.warehouse AS location,
             b.actual_qty AS qoh_location,
             (SELECT SUM(actual_qty) FROM `tabBin` WHERE item_code = i.name) AS tqoh_all_locations,
-            i.retail_price AS retail_price,
-            i.standard_cost AS cost,
-            si.qty_sold AS qty_sold
+            ip.price_list_rate AS retail_price,
+            i.valuation_rate AS cost,
+            SUM(sii.qty) AS qty_sold
         FROM
-            `tabSales Order` AS so
+            `tabSales Invoice` AS si
         JOIN
-            `tabSales Order Item` AS si ON so.name = si.parent
+            `tabSales Invoice Item` AS sii ON si.name = sii.parent
         JOIN
-            `tabItem` AS i ON si.item_code = i.name
+            `tabItem` AS i ON sii.item_code = i.name
         JOIN
-            `tabBin` AS b ON i.name = b.item_code AND b.warehouse = %(warehouse)s
+            `tabItem Default` AS defaults ON defaults.parent = i.name
+        JOIN
+            `tabBin` AS b ON i.name = b.item_code AND b.warehouse = sii.warehouse
+        JOIN
+            `tabItem Price` ip on ip.item_code = i.item_code and ip.price_list = "RET - Camo"
         WHERE
-            so.docstatus = 1  -- Approved sales orders
-            AND so.transaction_date BETWEEN %(from_date)s AND %(to_date)s
-    """, {"warehouse": warehouse, "from_date": from_date, "to_date": to_date}, as_dict=True)
-
-    # Prepare data for the report
-    for order in sales_orders:
-        data.append({
-            "sales_order": order.sales_order,
-            "transaction_date": order.transaction_date,
-            "customer": order.customer,
-            "item_code": order.item_code,
-            "quantity": order.quantity,
-            "rate": order.rate,
-            "amount": order.amount,
-            "brand": order.brand,
-            "supplier": order.supplier,
-            "retail_sku": order.retail_sku,
-            "qoh_location": order.qoh_location,
-            "tqoh_all_locations": order.tqoh_all_locations,
-            "retail_price": order.retail_price,
-            "cost": order.cost,
-            "qty_sold": order.qty_sold,
-        })
+            si.docstatus = 1 AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
+    """ + where_extra + 
+    """
+		GROUP BY 
+			item_code, brand, supplier, retail_sku, location, qoh_location, 
+			tqoh_all_locations, retail_price, cost
+    """, {"from_date": from_date, "to_date": to_date}, as_dict=True)
 
     return columns, data
