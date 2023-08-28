68b7cace2f3ce63800a2c47f47114f89d2180e7b
Aisenyi <malisa.aisenyi@gmail.com>
2023-08-29 01:24:34 +0300

    fix: Fixed supplier registering in matrial request

--- metactical/custom_scripts/material_request/material_request.py
+++ metactical/custom_scripts/material_request/material_request.py
@@ -5,8 +5,10 @@ def before_save(self, method):
 	if len(self.items) > 0:
 		set_default_supplier(self)
 		suppliers = ''
+		unique_suppliers = []
 		for item in self.items:
-			if item.ais_default_supplier:
+			if item.ais_default_supplier and item.ais_default_supplier not in unique_suppliers:
+				unique_suppliers.append(item.ais_default_supplier)
 				if suppliers != '':
 					suppliers += ', ' + item.ais_default_supplier
 				else:
