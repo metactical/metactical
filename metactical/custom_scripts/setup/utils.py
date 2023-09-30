9c7c5fccc3893fa27a657f8f365d5834b0c3208b
Aisenyi <malisa.aisenyi@gmail.com>
2023-09-30 10:55:51 +0300

    fix: Fix exchange rate api changes causing exchange rates not auto collected

---
+++ metactical/custom_scripts/setup/utils.py
@@ -0,0 +1,66 @@
+import frappe
+from frappe import _
+from frappe.utils import add_days, flt, get_datetime_str, nowdate
+
+@frappe.whitelist()
+def get_exchange_rate(from_currency, to_currency, transaction_date=None, args=None):
+	if not (from_currency and to_currency):
+		# manqala 19/09/2016: Should this be an empty return or should it throw and exception?
+		return
+	if from_currency == to_currency:
+		return 1
+
+	if not transaction_date:
+		transaction_date = nowdate()
+	currency_settings = frappe.get_doc("Accounts Settings").as_dict()
+	allow_stale_rates = currency_settings.get("allow_stale")
+
+	filters = [
+		["date", "<=", get_datetime_str(transaction_date)],
+		["from_currency", "=", from_currency],
+		["to_currency", "=", to_currency],
+	]
+
+	if args == "for_buying":
+		filters.append(["for_buying", "=", "1"])
+	elif args == "for_selling":
+		filters.append(["for_selling", "=", "1"])
+
+	if not allow_stale_rates:
+		stale_days = currency_settings.get("stale_days")
+		checkpoint_date = add_days(transaction_date, -stale_days)
+		filters.append(["date", ">", get_datetime_str(checkpoint_date)])
+
+	# cksgb 19/09/2016: get last entry in Currency Exchange with from_currency and to_currency.
+	entries = frappe.get_all(
+		"Currency Exchange", fields=["exchange_rate"], filters=filters, order_by="date desc", limit=1
+	)
+	if entries:
+		return flt(entries[0].exchange_rate)
+
+	try:
+		cache = frappe.cache()
+		key = "currency_exchange_rate_{0}:{1}:{2}".format(transaction_date, from_currency, to_currency)
+		value = cache.get(key)
+
+		if not value:
+			import requests
+
+			api_url = "http://api.exchangerate.host/convert"
+			response = requests.get(
+				api_url, params={"date": transaction_date, "from": from_currency, "to": to_currency, 
+					"access_key": "d2792ef5cbaa18d87550feb1c12afa58", "amount": 1}
+			)
+			# expire in 6 hours
+			response.raise_for_status()
+			value = response.json()["result"]
+			cache.setex(name=key, time=21600, value=flt(value))
+		return flt(value)
+	except Exception:
+		frappe.log_error(title="Get Exchange Rate")
+		frappe.msgprint(
+			_(
+				"Unable to find exchange rate for {0} to {1} for key date {2}. Please create a Currency Exchange record manually"
+			).format(from_currency, to_currency, transaction_date)
+		)
+		return 0.0
