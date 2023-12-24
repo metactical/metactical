550f559880640461d60e38ef684ca626838d06b2
Aisenyi <malisa.aisenyi@gmail.com>
2023-12-24 22:02:55 +0300

    Fix: Fix different rounding method for frontend and backend calculations. Also fixed return payment function in sales invoice

--- metactical/__init__.py
+++ metactical/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from metactical.custom_scripts.controllers.taxes_and_totals import custom_calculate_taxes_and_totals
+from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
 
 __version__ = '0.0.1'
 
+#set_total_amount_to_default_mop = custom_default_mop
+calculate_taxes_and_totals.set_total_amount_to_default_mop = custom_calculate_taxes_and_totals.set_total_amount_to_default_mop
