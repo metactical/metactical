12e526ca9080c11524bfbfec55ba4e45327a4fb0
Aisenyi <malisa.aisenyi@gmail.com>
2024-02-13 12:38:42 +0300

    chore: Added End of Day closing document

---
+++ metactical/metactical/doctype/end_of_day_closing/end_of_day_closing.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2024, Techlift Technologies and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class EndofDayClosing(Document):
+	pass
+
+@frappe.whitelist()
+def get_data(closing_date="2023-01-10", user="whatever", pos_profile="Victoria Operators"):
+	mode_of_payments = {}
+	invoices = []
+	expected_cash = 0
+	
+	payments = frappe.db.sql("""
+				SELECT
+					SUM(payment_reference.allocated_amount) AS amount_paid,
+					payment_entry.mode_of_payment, mop.type
+				FROM
+					`tabPayment Entry Reference` AS payment_reference
+				LEFT JOIN
+					`tabPayment Entry` AS payment_entry ON payment_reference.parent = payment_entry.name
+				LEFT JOIN
+					`tabSales Invoice` AS invoice ON invoice.name = payment_reference.reference_name
+				LEFT JOIN
+					`tabMode of Payment` AS mop ON mop.name = payment_entry.mode_of_payment
+				WHERE
+					payment_entry.posting_date = %(closing_date)s AND payment_entry.payment_type = 'Receive'
+					AND payment_reference.reference_doctype = "Sales Invoice" AND invoice.pos_profile = %(pos_profile)s
+				GROUP BY 
+					payment_entry.mode_of_payment, type
+				""", {"closing_date": closing_date, "pos_profile": pos_profile}, as_dict=1)
+				
+	pos_payments = frappe.db.sql("""
+				SELECT
+					SUM(payment.amount) AS amount_paid, 
+					payment.mode_of_payment, mop.type
+				FROM
+					`tabSales Invoice Payment` AS payment
+				LEFT JOIN
+					`tabSales Invoice` AS invoice ON invoice.name = payment.parent
+				LEFT JOIN
+					`tabMode of Payment` AS mop ON mop.name = payment.mode_of_payment
+				WHERE
+					invoice.posting_date = %(closing_date)s AND invoice.pos_profile = %(pos_profile)s
+				GROUP BY
+					payment.mode_of_payment, type
+				""", {"closing_date": closing_date, "pos_profile": pos_profile}, as_dict=1)
+	
+	for payment in payments:
+		mode_of_payments[payment.mode_of_payment] = mode_of_payments.get(payment.mode_of_payment, 0) + payment.amount_paid
+		if payment.type == "Cash":
+			expected_cash += payment.amount_paid
+	
+	for payment in pos_payments:
+		mode_of_payments[payment.mode_of_payment] = mode_of_payments.get(payment.mode_of_payment, 0) + payment.amount_paid
+		if payment.type == "Cash":
+			expected_cash += payment.amount_paid
+				
+	invoices = frappe.db.sql("""
+				SELECT
+					payment_reference.reference_doctype, payment_reference.reference_name, 
+					SUM(payment_reference.allocated_amount) AS amount_paid,
+					invoice.outstanding_amount AS owing
+				FROM
+					`tabPayment Entry Reference` AS payment_reference
+				LEFT JOIN
+					`tabPayment Entry` AS payment_entry ON payment_reference.parent = payment_entry.name
+				LEFT JOIN
+					`tabSales Invoice` AS invoice ON invoice.name = payment_reference.reference_name
+				WHERE
+					payment_entry.posting_date = %(closing_date)s AND payment_entry.payment_type = 'Receive'
+					AND payment_reference.reference_doctype = "Sales Invoice" AND invoice.pos_profile = %(pos_profile)s
+				GROUP BY
+					reference_doctype, reference_name, owing
+				""", {"closing_date": closing_date, "pos_profile": pos_profile}, as_dict=1)
+				
+	pos_invoices = frappe.db.sql("""
+				SELECT
+					"Sales Invoice" AS reference_doctype, invoice.name AS reference_name,
+					invoice.outstanding_amount AS owing, 
+					invoice.paid_amount AS amount_paid
+				FROM
+					`tabSales Invoice Payment` AS payment
+				LEFT JOIN
+					`tabSales Invoice` AS invoice ON invoice.name = payment.parent
+				WHERE
+					invoice.posting_date = %(closing_date)s AND invoice.pos_profile = %(pos_profile)s
+				GROUP BY
+					reference_doctype, reference_name, owing, amount_paid
+				""", {"closing_date": closing_date, "pos_profile": pos_profile}, as_dict=1)
+				
+	invoices = invoices + pos_invoices
+	
+	return {"payments": mode_of_payments, "invoices": invoices, "expected_cash": expected_cash}
+				
+	
