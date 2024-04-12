a5cf04919d76c919b4d913cff97360957306f3a4
nebiye <lioulnebiye@gmail.com>
2024-04-12 23:01:09 +0300

    code cleanup

--- metactical/custom_scripts/task/task.js
+++ metactical/custom_scripts/task/task.js
@@ -1,6 +1,5 @@
 frappe.ui.form.on('Task', {
     onload: function(frm) {
-        console.log("test")
         setTimeout(() => {
             $(".form-dashboard-section.form-links").hide();
         }, 10);
