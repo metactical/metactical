c8133f5c5f8b5402ef32b58e0bc6a3f0a10f5267
nebiye <lioulnebiye@gmail.com>
2024-05-30 16:14:07 +0300

    export stock items with their rate

--- metactical/custom_scripts/warehouse/warehouse.js
+++ metactical/custom_scripts/warehouse/warehouse.js
@@ -10,6 +10,19 @@ frappe.ui.form.on("Warehouse", {
           
         },
       });
-    });
+    }, __("Export"));
+
+    frm.add_custom_button(__("Export Stock Items With Rate"), function () {
+      frappe.call({
+        method: "metactical.custom_scripts.warehouse.warehouse.export_items_with_price_list",
+        args: {
+          warehouse: frm.doc.name,
+        },
+        callback: function (r) {
+          
+        },
+      });
+    }, __("Export"));
   },
+  
 });
