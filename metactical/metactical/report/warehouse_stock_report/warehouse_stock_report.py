1cf28fae58b22c1b633dbee35ef210aaf2411ea1
Aisenyi <malisa.aisenyi@gmail.com>
2024-06-01 02:30:28 +0300

    chore: Only get items actual amount > 0 in warehouse stock report

--- metactical/metactical/report/warehouse_stock_report/warehouse_stock_report.py
+++ metactical/metactical/report/warehouse_stock_report/warehouse_stock_report.py
@@ -27,7 +27,7 @@ def get_data(filters):
 			LEFT JOIN 
 				`tabItem` ON `tabItem`.item_code = `tabBin`.item_code
 			WHERE 
-				warehouse = %s
+				warehouse = %s AND actual_qty > 0
 			LIMIT %s OFFSET %s
 		""", (warehouse, limit, start), as_dict=1)
 
