866f1fc527645573d4990e730c3f64d374ffd479
Aisenyi <malisa.aisenyi@gmail.com>
2024-07-11 15:51:35 +0300

    fix: Fix patch script for large no of item prices

--- metactical/patches/copy_retailsku_item_price.py
+++ metactical/patches/copy_retailsku_item_price.py
@@ -1,6 +1,10 @@
 import frappe
 
 def execute():
-	item_prices = frappe.get_all('Item Price', fields=['name', 'item_code'])
-	for item_price in item_prices:
-		frappe.db.set_value('Item Price', item_price.name, 'ifw_retailskusuffix', frappe.db.get_value('Item', item_price.item_code, 'ifw_retailskusuffix'))
+	frappe.db.sql("""
+		UPDATE
+			   `tabItem Price` item_price
+		INNER JOIN
+			`tabItem` AS item ON item_price.item_code = item.name
+		SET item_price.ifw_retailskusuffix = item.ifw_retailskusuffix
+		""")
