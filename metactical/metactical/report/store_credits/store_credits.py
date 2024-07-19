edb55c698c395b0f0e44b80d33ee02854b93228b
nebiye <lioulnebiye@gmail.com>
2024-07-19 19:39:20 +0300

    separting retun docs to let them have one row for each

--- metactical/metactical/report/store_credits/store_credits.py
+++ metactical/metactical/report/store_credits/store_credits.py
@@ -67,19 +67,15 @@ def get_data(customers, sales_invoice):
 				amount = 0
 			else: continue
 
-		credit_docs = get_credit_docs(customer, sales_invoice)
-		row = {
-			"customer": "<a href='/app/customer/{0}' _target='blank'>{0}</a>".format(customer),
-			"credit": amount, 
-			"store_credit_no": credit_docs
-		}
+		rows = get_credit_docs(customer, sales_invoice)
 
 		contact = get_customer_email_and_phone(customer)
 		if contact:
-			row["email"] = contact[0].get('email_id')
-			row["mobile"] = contact[0].get('mobile_no') if contact[0].get('mobile_no') else contact[0].get('phone')
+			for row in rows:
+				row["email"] = contact[0].get('email_id')
+				row["mobile"] = contact[0].get('mobile_no') if contact[0].get('mobile_no') else contact[0].get('phone')
 
-		data.append(row)
+		data += rows
 	
 	return data
 
@@ -90,24 +86,25 @@ def get_credit_docs(customer, sales_invoice):
 	if sales_invoice:
 		sales_invoice_filter = f"AND against_voucher = '{sales_invoice}'"
 
-	credits = frappe.db.sql(f"""
+	credits = frappe.db.sql(
+		f"""
 		SELECT against_voucher, voucher_type, voucher_no, debit_in_account_currency, credit_in_account_currency
 		FROM `tabGL Entry`
 		WHERE party = '{customer}'
-		{sales_invoice_filter}	
+		{sales_invoice_filter}
 		AND is_cancelled = 0 and posting_date >= '{last_two_years}'
-	""", as_dict=True)
+		""", as_dict=True)
 
-	# group by against_voucher 
+	# group by against_voucher
 	against_voucher_group = {}
 	for credit in credits:
 		if credit.against_voucher not in against_voucher_group:
 			against_voucher_group[credit.against_voucher] = []
-		
+
 		against_voucher_group[credit.against_voucher].append(credit)
 
-
-	# move jounrnal entries without against voucher but voucher_no matches with sales invoice
+	# move journal entries without against voucher but voucher_no matches with sales invoice
+	rows = []
 	docs_without_against_voucher = []
 	if None in against_voucher_group:
 		for v in against_voucher_group[None]:
@@ -117,39 +114,41 @@ def get_credit_docs(customer, sales_invoice):
 				docs_without_against_voucher.append(v)
 
 		if len(docs_without_against_voucher) > 0:
-			against_voucher_group["None"] = docs_without_against_voucher 
+			against_voucher_group["None"] = docs_without_against_voucher
 			# remove the None key
 			against_voucher_group.pop(None)
 
-
-	# filter out unpaid docs from documents in the name of the cusotmer
+	# filter out unpaid docs from documents in the name of the customer
 	unpaid_docs = []
 	logger.info(against_voucher_group)
 	for invoice, vouchers in against_voucher_group.items():
-		if invoice == "None": # gl entries without against voucher (not attached to a sales invoice)
+		if invoice == "None":  # gl entries without against voucher (not attached to a sales invoice)
 			for voucher in vouchers:
 				if voucher.credit_in_account_currency > 0:
 					if voucher.voucher_type == "Journal Entry":
 						accounts = frappe.get_doc("Journal Entry", voucher.voucher_no).accounts
 						for account in accounts:
 							if account.debit_in_account_currency > 0 and account.reference_type == "Sales Invoice":
-								return_si = frappe.db.get_value("Sales Invoice", 
-																	{
-																		"return_against": account.reference_name, 
-																		"neb_store_credit_beneficiary": customer,
-																		"grand_total": -1 * account.debit_in_account_currency
-																	}, "name")
+								return_si = frappe.db.get_value(
+									"Sales Invoice",
+									{
+										"return_against": account.reference_name,
+										"neb_store_credit_beneficiary": customer,
+										"grand_total": -1 * account.debit_in_account_currency,
+									},
+									"name",
+								)
 
 								if return_si:
 									voucher.voucher_no = return_si
 									voucher.voucher_type = "Sales Invoice"
 									unpaid_docs.append(voucher)
 
-					elif voucher.voucher_type == "Payment Entry": # display the linked sales
+					elif voucher.voucher_type == "Payment Entry":  # display the linked sales
 						voucher = get_linked_doc_from_payment_entry(voucher)
 						if voucher:
 							unpaid_docs.append(voucher)
-							
+
 					else:
 						unpaid_docs.append(voucher)
 		else:
@@ -161,22 +160,24 @@ def get_credit_docs(customer, sales_invoice):
 
 			elif sum_credit > sum_debit:
 				for voucher in vouchers:
-					if voucher.credit_in_account_currency > 0 and len(vouchers) == 2: # the amount paid is greater than invoice amount
+					if voucher.credit_in_account_currency > 0 and len(vouchers) == 2:  # the amount paid is greater than invoice amount
 						unpaid_docs.append(voucher)
-								
-					elif voucher.credit_in_account_currency > 0 and len(vouchers) == 1: # store credit given by a journal entry
+
+					elif voucher.credit_in_account_currency > 0 and len(vouchers) == 1:  # store credit given by a journal entry
 						if voucher.voucher_type == "Payment Entry":
 							voucher = get_linked_doc_from_payment_entry(voucher)
 							if voucher:
 								unpaid_docs.append(voucher)
 
-					elif voucher.credit_in_account_currency > 0 and len(vouchers) > 2: # for return documents and journal entries
+					elif voucher.credit_in_account_currency > 0 and len(vouchers) > 2:  # for return documents and journal entries
 						if voucher.voucher_type in ["Sales Invoice"] and voucher.voucher_no != invoice:
 							unpaid_docs.append(voucher)
 						elif voucher.voucher_type in ["Journal Entry"]:
 							unpaid_docs.append(voucher)
 						elif voucher.voucher_type in ["Payment Entry"]:
-							sum_of_payment_entries = sum([voucher.credit_in_account_currency for voucher in vouchers if voucher.voucher_type == "Payment Entry" and voucher.credit_in_account_currency > 0])
+							sum_of_payment_entries = sum(
+								[voucher.credit_in_account_currency for voucher in vouchers if voucher.voucher_type == "Payment Entry" and voucher.credit_in_account_currency > 0]
+							)
 							if sum_of_payment_entries == vouchers[0].debit_in_account_currency:
 								continue
 							elif vouchers[0].debit_in_account_currency == voucher.credit_in_account_currency:
@@ -186,25 +187,32 @@ def get_credit_docs(customer, sales_invoice):
 								if voucher:
 									unpaid_docs.append(voucher)
 
-
-	docs = ""
 	if sales_invoice:
-		docs = f"<a href='/app/sales-invoice/{sales_invoice}' _target='blank'>{sales_invoice}</a>"
+		rows.append(
+			{
+				"customer": customer,
+				"credit": sum([ud.credit_in_account_currency for ud in unpaid_docs]),
+				"original_credit": sum([ud.debit_in_account_currency for ud in unpaid_docs]),
+				"store_credit_no": f"<a href='/app/sales-invoice/{sales_invoice}' _target='blank'>{sales_invoice}</a>",
+			}
+		)
 	else:
 		for ud in unpaid_docs:
 			if ud.voucher_type == "Sales Invoice":
-				docs += f"<a href='/app/sales-invoice/{ud.voucher_no}' _target='blank'>{ud.voucher_no}</a>, "
-			elif ud.voucher_type == "Journal Entry":
-				docs += f"<a href='/app/journal-entry/{ud.voucher_no}' _target='blank'>{ud.voucher_no}</a>, "
-			elif ud.voucher_type == "Payment Entry":
-				docs += f"<a href='/app/payment-entry/{ud.voucher_no}' _target='blank'>{ud.voucher_no}</a>, "
-			elif ud.voucher_type == "Sales Order":
-				docs += f"<a href='/app/sales-order/{ud.voucher_no}' _target='blank'>{ud.voucher_no}</a>, "
+				link = f"<a href='/app/sales-invoice/{ud.voucher_no}' _target='blank'>{ud.voucher_no}</a>"
+				
+				original_store_credit = frappe.db.get_value("Sales Invoice", ud.voucher_no, "grand_total")
 
-		if docs.endswith(", "):
-			docs = docs[:-2]
+				rows.append(
+					{
+						"customer": customer,
+						"credit": ud.credit_in_account_currency,
+						"original_credit": original_store_credit,
+						"store_credit_no": link,
+					}
+				)
 
-	return docs
+	return rows
 
 def get_linked_doc_from_payment_entry(voucher):
 	payment_entry = frappe.get_doc("Payment Entry", voucher.voucher_no)
