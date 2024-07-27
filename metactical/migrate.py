8e682d80fe83e857a47df5efb9095ef0220ba77f
Aisenyi <malisa.aisenyi@gmail.com>
2024-07-27 21:04:08 +0300

    fix: Changing customeer naming series through script so it's not overwritten on migrate

--- metactical/migrate.py
+++ metactical/migrate.py
@@ -1,6 +1,27 @@
 import frappe
 
 def after_migrate():
+	reset_po_naming_series()
+	reset_customer_naming_series()
+
+def reset_customer_naming_series():
+	exists = frappe.db.exists('Property Setter', {'doctype_or_field': 'DocField', 'doc_type': 'Customer',
+					'field_name': 'naming_series', 'property': 'options'})
+	if exists:
+		frappe.db.delete('Property Setter', exists)
+
+	doc = frappe.new_doc('Property Setter')
+	doc.update({
+		'doctype_or_field': 'DocField',
+		'doc_type': 'Customer',
+		'field_name': 'naming_series',
+		'property': 'options',
+		'value': 'CS-.YY.-'
+	})
+	doc.insert()
+	
+
+def reset_po_naming_series():
 	#Reset naming series in PO based on site name
 	exists = frappe.db.exists('Property Setter', {'doctype_or_field': 'DocField', 'doc_type': 'Purchase Order',
 					'field_name': 'naming_series', 'property': 'options'})
@@ -22,9 +43,3 @@ def after_migrate():
 		'value': value
 	})
 	doc.insert()
-
-	update_payment_completed_at()
-
-def update_payment_completed_at():
-	# update neb_payment_completed_at in Sales Order
-	frappe.db.sql("""UPDATE `tabSales Order` SET neb_payment_completed_at=transaction_date WHERE neb_payment_completed_at is NULL and billing_status='Fully Billed'""")
