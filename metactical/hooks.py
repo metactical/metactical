027e003b71c051292cabfc9abc867db71f6eebbb
Aisenyi <malisa.aisenyi@gmail.com>
2024-08-04 23:14:13 +0300

    chore: changed jinja methods to version-14 compatible

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -234,12 +234,12 @@ fixtures = [{
 ]
 
 #For using in print format
-jenv = {
+jinja = {
 	"methods": [
-		"get_po_items:metactical.custom_scripts.purchase_order.purchase_order.get_po_items",
-		"get_pr_items:metactical.custom_scripts.purchase_receipt.purchase_receipt.get_pr_items",
-		"get_barcode:metactical.barcode_generator.get_barcode",
-		"si_mode_of_payment:metactical.custom_scripts.sales_invoice.sales_invoice.si_mode_of_payment"
+		"metactical.custom_scripts.purchase_order.purchase_order.get_po_items",
+		"metactical.custom_scripts.purchase_receipt.purchase_receipt.get_pr_items",
+		"metactical.barcode_generator.get_barcode",
+		"metactical.custom_scripts.sales_invoice.sales_invoice.si_mode_of_payment"
 	]
 }
 
