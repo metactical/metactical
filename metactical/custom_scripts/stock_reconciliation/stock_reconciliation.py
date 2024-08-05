c1e6e175113e52a37971367df8043e60083554ab
nebiye <lioulnebiye@gmail.com>
2024-08-06 02:01:50 +0300

    code cleanup

--- metactical/custom_scripts/stock_reconciliation/stock_reconciliation.py
+++ metactical/custom_scripts/stock_reconciliation/stock_reconciliation.py
@@ -11,7 +11,6 @@ class CustomStockReconciliation(StockReconciliation):
 					"The task has been enqueued as a background job. In case there is any issue on processing in background, the system will add a comment about the error on this Stock Reconciliation and revert to the Draft stage"
 				)
 			)
-
 			self.queue_action("submit", timeout=2000)
 		else:
 			self._submit()
