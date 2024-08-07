838943941ad3fb652dfb485f162abc79c54e00d0
Aisenyi <malisa.aisenyi@gmail.com>
2024-08-07 21:17:12 +0300

    fix: Removed unused imports in Sales Invoice

--- metactical/custom_scripts/sales_invoice/sales_invoice.py
+++ metactical/custom_scripts/sales_invoice/sales_invoice.py
@@ -2,11 +2,7 @@ import frappe
 from frappe import _, msgprint
 import barcode as _barcode
 from io import BytesIO
-from frappe.model.mapper import get_mapped_doc, map_child_doc
-from frappe.utils import nowdate, cstr, flt, cint, now, getdate
-from erpnext.setup.doctype.company.company import update_company_current_month_sales
-from erpnext.accounts.doctype.sales_invoice.sales_invoice import unlink_inter_company_doc
-from erpnext.healthcare.utils import manage_invoice_submit_cancel
+from frappe.utils import now
 from erpnext.accounts.doctype.sales_invoice.sales_invoice import SalesInvoice
 from erpnext.controllers.selling_controller import SellingController
 from erpnext.controllers.stock_controller import StockController
