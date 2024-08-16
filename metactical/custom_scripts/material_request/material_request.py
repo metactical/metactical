5813f23157af94f6e2c3209d6fe5ce57b3f06918
nebiye <lioulnebiye@gmail.com>
2024-08-16 21:58:22 +0300

    merge conflict fix

--- metactical/custom_scripts/material_request/material_request.py
+++ metactical/custom_scripts/material_request/material_request.py
@@ -1,25 +1,53 @@
 import frappe
+from erpnext.stock.doctype.material_request.material_request import MaterialRequest
+from erpnext.stock.utils import get_stock_balance
 import json
 
-def before_save(self, method):
-	self.set_status(update=True)	
-	if len(self.items) > 0:
-		set_default_supplier(self)
-		suppliers = ''
-		unique_suppliers = []
-		for item in self.items:
-			if item.ais_default_supplier and item.ais_default_supplier not in unique_suppliers:
-				unique_suppliers.append(item.ais_default_supplier)
-				if suppliers != '':
-					suppliers += ', ' + item.ais_default_supplier
-				else:
-					suppliers += item.ais_default_supplier
-		self.ais_suppliers = suppliers
+class CustomMaterialRequest(MaterialRequest):
+	def before_submit(self):
+		super(CustomMaterialRequest, self).before_submit()
+		self.validate_item_qty()
+		self.validate_qoh()
 
-	# check if a qty greater than the quantity on hand is entered
-	for item in self.items:
-		if item.qty > item.qoh:
-			frappe.throw("Quantity entered for item <b>{0}</b> is greater than the available quantity (<b>{1}</b>) in the warehouse".format(item.item_code, item.qoh))
+	def validate_item_qty(self):
+		if self.material_request_type == 'Material Transfer':
+			for item in self.items:
+				if item.from_warehouse is None or item.from_warehouse == '':
+					frappe.throw(f'Source Warehouse is required for item {item.item_code}')
+
+				if item.qty > get_stock_balance(item.item_code, item.from_warehouse):
+					frappe.throw(f'Requested quantity of item {item.item_code} is greater than available quantity at warehouse')
+
+	def validate_qoh(self):
+		# check if a qty greater than the quantity on hand is entered
+		for item in self.items:
+			if item.qty > item.qoh:
+				frappe.throw("Quantity entered for item <b>{0}</b> is greater than the available quantity (<b>{1}</b>) in the warehouse".format(item.item_code, item.qoh))
+
+	def before_save(self):
+		self.set_status(update=True)	
+		if len(self.items) > 0:
+			self.set_default_supplier()
+			suppliers = ''
+			unique_suppliers = []
+			for item in self.items:
+				if item.ais_default_supplier and item.ais_default_supplier not in unique_suppliers:
+					unique_suppliers.append(item.ais_default_supplier)
+					if suppliers != '':
+						suppliers += ', ' + item.ais_default_supplier
+					else:
+						suppliers += item.ais_default_supplier
+			self.ais_suppliers = suppliers
+
+		self.validate_qoh()
+
+	def set_default_supplier(self):
+		for item in self.items:
+			if not item.ais_default_supplier:
+				doc = frappe.get_doc('Item', item.item_code)
+				for defaults in doc.item_defaults:
+					if defaults.company == self.company:
+						item.ais_default_supplier = defaults.default_supplier
 
 def set_default_supplier(self):
 	for item in self.items:
