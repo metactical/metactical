acad0a196725614a0aaab3c48b819650702bab00
Aisenyi <malisa.aisenyi@gmail.com>
2024-08-23 18:00:35 +0300

    chore: Removed customization to update reserved quantity on sales order items update

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -198,7 +198,6 @@ after_migrate = "metactical.migrate.after_migrate"
 override_whitelisted_methods = {
 	"erpnext.selling.doctype.sales_order.sales_order.create_pick_list": "metactical.custom_scripts.pick_list.pick_list.create_pick_list",
 	"frappe.utils.print_format.download_pdf": "metactical.print_format.download_pdf",
-	#"erpnext.controllers.accounts_controller.update_child_qty_rate": "metactical.custom_scripts.sales_order_item.sales_order_item.update_child_qty_rate",
 	"erpnext.stock.doctype.pick_list.pick_list.create_delivery_note": "metactical.custom_scripts.pick_list.pick_list.create_delivery_note",
 	"erpnext.stock.get_item_details.get_item_details": "metactical.custom_scripts.get_item_details.get_item_details",
 	"erpnext.selling.doctype.sales_order.sales_order.make_sales_invoice": "metactical.custom_scripts.sales_order.sales_order.make_sales_invoice",
