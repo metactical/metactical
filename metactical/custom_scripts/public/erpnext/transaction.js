e14957c5b5e8acf71b25b35a07d2473cec0147b7
Aisenyi <malisa.aisenyi@gmail.com>
2024-08-30 22:25:51 +0300

    fix: Removed metactical customization to play sound after item scan

--- metactical/custom_scripts/public/erpnext/transaction.js
+++ metactical/custom_scripts/public/erpnext/transaction.js
@@ -1,34 +1,4 @@
 erpnext.TransactionController = class TransactionController extends erpnext.TransactionController{
-	scan_barcode() {
-		let me = this;
-
-		if(this.frm.doc.scan_barcode) {
-			frappe.call({
-				method: "erpnext.selling.page.point_of_sale.point_of_sale.search_for_serial_or_batch_or_barcode_number",
-				args: {
-					search_value: this.frm.doc.scan_barcode
-				}
-			}).then(r => {
-				const data = r && r.message;
-				// MEtactical Customization: Added sound on scan of barcode 
-				if (!data || Object.keys(data).length === 0) {
-					frappe.utils.play_sound("error");
-					frappe.show_alert({
-						message: __('Cannot find Item with this Barcode'),
-						indicator: 'red'
-					});
-					return;
-				}
-				else{
-					frappe.utils.play_sound("alert");
-				}
-
-				me.modify_table_after_scan(data);
-			});
-		}
-		return false;
-	}
-	
 	// Metactical Customization: Add scan to confirm button functionality
 	ais_scan_to_confirm() {
 		var me = this;
