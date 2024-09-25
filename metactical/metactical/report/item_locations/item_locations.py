9a8fb815639c73e727268d6bab070ca61cbaa41a
nebiye lioul <lioulnebiye@gmail.com>
2024-09-26 02:03:10 +0300

    added supplier in the Item locations report

--- metactical/metactical/report/item_locations/item_locations.py
+++ metactical/metactical/report/item_locations/item_locations.py
@@ -6,22 +6,30 @@ import frappe
 def execute(filters=None):
 	purchase_orders = filters.get("purchase_order") if filters.get("purchase_order") else None
 	purchase_receipts = filters.get("purchase_receipt") if filters.get("purchase_receipt") else None
+	supplier = filters.get("supplier") if filters.get("supplier") else None
 
-	if not purchase_orders and not purchase_receipts:
+	if not purchase_orders and not purchase_receipts and not supplier:
 		return [], []
 
-	columns = get_columns(purchase_orders, purchase_receipts)
-	data = get_data(purchase_orders, purchase_receipts)
+	columns = get_columns(purchase_orders, purchase_receipts, supplier)
+	data = get_data(purchase_orders, purchase_receipts, supplier)
 	return columns, data
 
-def get_data(purchase_orders, purchase_receipts):
-	doctype = "Purchase Order" if purchase_orders else "Purchase Receipt"
-	docnames = purchase_orders if purchase_orders else purchase_receipts
+def get_data(purchase_orders, purchase_receipts, supplier):
+	doctype = ""
+	if purchase_orders or purchase_receipts:
+		doctype = "Purchase Order" if purchase_orders else "Purchase Receipt"
+		docnames = purchase_orders if purchase_orders else purchase_receipts		
 
-	items = frappe.db.sql(f""" SELECT doc.item_code, i.ifw_location, doc.parent as name, i.ifw_retailskusuffix as retail_sku
-								FROM `tab{doctype} Item` doc
-								JOIN `tabItem` i on doc.item_code = i.name
-								WHERE doc.parent IN %s """, (docnames,), as_dict=True) 
+		items = frappe.db.sql(f""" SELECT doc.item_code, i.ifw_location, doc.parent as name, i.ifw_retailskusuffix as retail_sku
+									FROM `tab{doctype} Item` doc
+									JOIN `tabItem` i on doc.item_code = i.name
+									WHERE doc.parent IN %s """, (docnames,), as_dict=True) 
+	elif supplier:
+		items = frappe.db.sql(""" SELECT item_code, ifw_location, supplier as name, ifw_retailskusuffix as retail_sku
+									FROM `tabItem Supplier`
+									JOIN `tabItem` on item_code = `tabItem Supplier`.parent
+									WHERE supplier = %s """, supplier, as_dict=True)
 	
 	data = []
 	for item in items:
@@ -37,7 +45,7 @@ def get_data(purchase_orders, purchase_receipts):
 	return data
 
 
-def get_columns(purchase_orders, purchase_receipts):
+def get_columns(purchase_orders, purchase_receipts, supplier):
 	columns = []
 
 	if purchase_orders:
@@ -55,6 +63,14 @@ def get_columns(purchase_orders, purchase_receipts):
 			"fieldtype": "Data",
 			"width": 200
 
+		})
+	
+	if supplier:
+		columns.append({
+			"label": "Supplier",
+			"fieldname": "document",
+			"fieldtype": "Data",
+			"width": 200
 		})
 
 	columns.append({
