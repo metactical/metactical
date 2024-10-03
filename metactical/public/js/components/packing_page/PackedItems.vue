0240be970d3fc9ab1e5729ca2b0009a1ac51bcaa
nebiye lioul <lioulnebiye@gmail.com>
2024-10-03 21:58:15 +0300

    layout for filters, pending items, packed items, current item is done in vue

---
+++ metactical/public/js/components/packing_page/PackedItems.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="packing-item" @click="selectItem">
+        <img :src="item.image" :alt="item.item_name" />
+        <div class="item-detail">
+            <h5 class="item-title">{{ item.item_name }}</h5>
+            <p class="item-description">{{ item.item_code }}</p>
+        </div>
+        <div class="item-quantity">
+            <span>X {{ item.qty }}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['item'],
+    methods: {
+        selectItem() {
+            console.log('selecting item', this.item);
+            this.$emit('select-item', this.item);
+        }
+    }
+}
+</script>
