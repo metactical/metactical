364bd26d49c484b04ef75c75d4a0cca2b6fd8705
nebiye lioul <lioulnebiye@gmail.com>
2024-10-04 22:49:45 +0300

    barcode generator created for print formats

--- metactical/barcode_generator.py
+++ metactical/barcode_generator.py
@@ -36,3 +36,22 @@ def get_barcode(name):
 	barcode.get('code128', name).write(rv, options={"write_text": False})
 	bstring = rv.getvalue()
 	return bstring.decode('ISO-8859-1')
+
+def get_barcode_for_print_format(name):
+	rv = BytesIO()
+	options = {
+		"write_text": True,  # Do not write text below the barcode
+		"module_height": 9,  # Height of the barcode
+		"dpi": 96,  # DPI for the image
+		"font_size": 6,  # Since we are not writing text
+		"module_width": 0.23,  # Adjust to fit unit as you mentioned `Fit`
+		"quiet_zone": 0,  # Quiet zone width in mm
+		"text_distance": 2.2,  # Padding between barcode and text
+		"background": "#ffffff",  # Background color
+		"foreground": "#000000",  # Barcode color
+		# "image_format": "GIF",  # Image format
+	}
+	
+	barcode.get('code128', name).write(rv, options=options)
+	bstring = rv.getvalue()
+	return bstring.decode('ISO-8859-1')
