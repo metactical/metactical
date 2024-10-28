e2237336b972b8eeed956bb4f350ddf988abf021
nebiye lioul <lioulnebiye@gmail.com>
2024-10-28 23:55:42 +0300

    Import preview for pricing rule import tool

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 app_include_css = ["metactical.bundle.scss", "/assets/metactical/css/metactical_task.css"]
-app_include_js = ["metactical.bundle.js", "/assets/metactical/js/metactical_kanban_custom.js"]
+app_include_js = ["metactical.bundle.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/metactical/css/metactical.css"
