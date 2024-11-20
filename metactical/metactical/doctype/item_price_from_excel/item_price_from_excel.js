792098837e8e46ed9582a36b1dd9dc64ac386997
nebiye lioul <lioulnebiye@gmail.com>
2024-11-20 23:01:21 +0300

    manual trigger for the item price import tool

--- metactical/metactical/doctype/item_price_from_excel/item_price_from_excel.js
+++ metactical/metactical/doctype/item_price_from_excel/item_price_from_excel.js
@@ -2,7 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Item Price From Excel', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__('Import'), function() {
+			frappe.call({
+				method: "runserverobj",
+				freeze: true,
+				args: {
+					docs: frm.doc,
+					method: "submit"
+				},
+			});
+		})
+	}
 });
