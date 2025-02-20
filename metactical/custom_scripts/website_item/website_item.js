fe492a837c487974375f33e6d3e19c0f0cd13196
nebiye lioul <lioulnebiye@gmail.com>
2025-02-20 14:22:41 +0300

    code cleanup

--- metactical/custom_scripts/website_item/website_item.js
+++
@@ -1,9 +0,0 @@
-cur_frm.fields_dict['neb_website_specifications'].grid.get_field('description_link').get_query = function(doc, cdt, cdn) {
-        var row = locals[cdt][cdn];
-        var options = ["", row.label]
-        return {
-            filters:[
-                    ['Website Specifications Description', 'label', 'in', options.join(",")]
-            ]
-        }
-}
