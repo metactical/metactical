4916927aa2f3579bf0e2b1316d60cf1066ae9241
nebiye lioul <lioulnebiye@gmail.com>
2025-03-15 16:35:34 +0300

    feat: update RabbitMQ consumer to include site parameter and refactor item price receiver method

--- metactical/custom_scripts/utils/rmq_consumer_methods.py
+++ metactical/custom_scripts/utils/rmq_consumer_methods.py
@@ -1,16 +1,15 @@
 import frappe
 
-def receive_item_price(*args, **kwargs):
+def receive_item_price(parsedContent):
 	try:
 		# get the file path
-		form_data = frappe.form_dict
-		item_code = form_data.get("item_code")
-		price_list_rate = form_data.get("price_list_rate")
-		price_list = form_data.get("price_list")
-		currency = form_data.get("currency")
-		valid_from = form_data.get("valid_from")
-		valid_to = form_data.get("valid_to")
-		uom = form_data.get("uom")
+		item_code = parsedContent.get("item_code")
+		price_list_rate = parsedContent.get("price_list_rate")
+		price_list = parsedContent.get("price_list")
+		currency = parsedContent.get("currency") 
+		valid_from = parsedContent.get("valid_from") or ""
+		valid_to = parsedContent.get("valid_to") or ""
+		uom = parsedContent.get("uom") if parsedContent.get('uom') != "None" else ""
 
 		item_price = frappe.db.exists("Item Price", {"item_code": item_code, "price_list": price_list})
 		if item_price:
@@ -36,8 +35,8 @@ def receive_item_price(*args, **kwargs):
 		frappe.db.commit()
 	except Exception as e:
 		message = ""
-		if form_data:
-			message = f"Item: {form_data.get('item')} \n Price List: {form_data.get('price_list')} \n Price: {form_data.get('price_list_rate')} \n Currency: {form_data.get('currency')} \n UOM: {form_data.get('uom')} \n Valid From: {form_data.get('valid_from')} \n Valid To: {form_data.get('valid_to')}"
+		if parsedContent:
+			message = f"Item: {parsedContent.get('item_code')} \n Price List: {parsedContent.get('price_list')} \n Price: {parsedContent.get('price_list_rate')} \n Currency: {parsedContent.get('currency')} \n UOM: {parsedContent.get('uom')} \n Valid From: {parsedContent.get('valid_from')} \n Valid To: {parsedContent.get('valid_to')}"
    
 		message += f"\n Error: {str(e)}"
 		frappe.log_error(title="Item Price Error", message=message)
