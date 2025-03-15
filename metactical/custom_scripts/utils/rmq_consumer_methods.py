142698a27724bc25a99010192a3f313c438ed2ed
nebiye lioul <lioulnebiye@gmail.com>
2025-03-15 20:57:49 +0300

    feat: refactor receive_item_price method to streamline item price updates and insertions

--- metactical/custom_scripts/utils/rmq_consumer_methods.py
+++ metactical/custom_scripts/utils/rmq_consumer_methods.py
@@ -12,25 +12,23 @@ def receive_item_price(parsedContent):
 		uom = parsedContent.get("uom") if parsedContent.get('uom') != "None" else ""
 
 		item_price = frappe.db.exists("Item Price", {"item_code": item_code, "price_list": price_list})
+		item_price_obj = {
+			"item_code": item_code,
+			"price_list": price_list,
+			"price_list_rate": price_list_rate,
+			"currency": currency,
+			"uom": uom,
+			"valid_from": valid_from,
+			"valid_to": valid_to
+		}
+
 		if item_price:
 			item_price = frappe.get_doc("Item Price", item_price)
-			item_price.price_list_rate = price_list_rate
-			item_price.currency = currency
-			item_price.uom = uom
-			item_price.valid_from = valid_from
-			item_price.valid_to = valid_to
+			item_price.update(item_price_obj)
 			item_price.save()
 		else:
-			item_price = frappe.get_doc({
-				"doctype": "Item Price",
-				"item_code": item_code,
-				"price_list": price_list,
-				"price_list_rate": price_list_rate,
-				"currency": currency,
-				"uom": uom,
-				"valid_from": valid_from,
-				"valid_to": valid_to
-			}).insert()
+			item_price_obj.update({"doctype": "Item Price"})
+			item_price = frappe.get_doc(item_price_obj).insert()
 
 		frappe.db.commit()
 	except Exception as e:
