3c10689558fcf244bddb129a5b9a58c0a8419251
nebiye lioul <lioulnebiye@gmail.com>
2025-03-24 22:31:26 +0300

    fix: use the wareshouse in the POS Profile in items

--- metactical/metactical/report/item_details_for_pos/item_details_for_pos.py
+++ metactical/metactical/report/item_details_for_pos/item_details_for_pos.py
@@ -23,7 +23,7 @@ def get_data(filters):
 	if not price_list:
 		frappe.throw("Please set default price list in Stock Settings or POS Profile")
   
-	warehouses = frappe.db.get_list("SB Warehouse Inventory Sync", filters={"parent": pos_profile.ifw_default_lead_source}, fields=["warehouse"])
+	warehouses = frappe.db.get_all("SB Warehouse Inventory Sync", filters={"parent": pos_profile.ifw_default_lead_source}, fields=["warehouse"])
 	if not warehouses:
 		frappe.throw("No warehouses found for the selected in the <a href='/app/lead-source/{0}'>Lead Source</a>".format(pos_profile.ifw_default_lead_source))
  
