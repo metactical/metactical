9cf00111c5a372f7e2280f37959e172ab228cfb8
nebiye lioul <lioulnebiye@gmail.com>
2025-04-16 16:28:30 +0300

    fix: update whitelist decorator for get_item_from_barcode to enhance security

--- metactical/custom_scripts/utils/pos_api.py
+++ metactical/custom_scripts/utils/pos_api.py
@@ -423,7 +423,7 @@ def get_comments(form_data):
         
     return comments
 
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def get_item_from_barcode(barcode, branch):
     item = frappe.db.sql(f"""
         SELECT 
