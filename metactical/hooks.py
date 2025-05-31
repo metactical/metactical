e1ce367c8ee42b2e331ec3e81340cd4182c818ea
Aisenyi <malisa.aisenyi@gmail.com>
2025-05-31 14:38:05 +0300

    chore: Small changes to shipment doctype for canada post shipments
    - Allow inclusion of weights lesser than 0.1
    - Saving the tracking number and shipment information in delivery Note
    - Save the shipping cost in the Shipment doctype

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -1346,7 +1346,8 @@ fixtures = [{
 			"Stock Ledger Entry-autoname",
 			"Sales Taxes and Charges-autoname",
 			"Bin-autoname",
-			"Custom Field-label-width"
+			"Custom Field-label-width",
+			"Shipment Parcel-weight-precision"
 		]]]
 	},
   	{
