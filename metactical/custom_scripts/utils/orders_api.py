de111fb529ecb17e7dbc448b8b19f9fca0555c64
nebiye lioul <lioulnebiye@gmail.com>
2025-07-03 00:18:03 +0300

    fix: Update Quebec tax return format in get_taxes_and_charges function

--- metactical/custom_scripts/utils/orders_api.py
+++ metactical/custom_scripts/utils/orders_api.py
@@ -497,7 +497,7 @@ def get_taxes_and_charges(province, country, company=None):
 	elif province == "Prince Edward Island":
 		return "Prince Edward Island - ICL"
 	elif province == "Quebec":
-		return "Quebec GST and QST - ICL"
+		return "Quebec - GST - ICL"
 	elif province == "Saskatchewan":
 		return "Saskatchewan - ICL"
 	elif province == "Northwest Territories":
