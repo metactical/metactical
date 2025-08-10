d0ddd4198d7e690048e1c3107d3e28f0caafb688
nebiye lioul <lioulnebiye@gmail.com>
2025-08-10 15:19:23 +0300

    feat: Enhance RabbitMQ Orders Log with re-sync functionality and improve logging in order processing

--- metactical/custom_scripts/contact/contact.py
+++ metactical/custom_scripts/contact/contact.py
@@ -3,6 +3,6 @@ import frappe
 def validate(self, method):
 	if len(self.phone_nos) > 0:
 		for row in self.phone_nos:
-			allowed = "1234567890-+()"
+			allowed = "1234567890-+() "
 			if not all(digit in allowed for digit in row.phone):
 				frappe.throw('Only numbers and characters +-() allowed in phone number field')
--- metactical/metactical/doctype/rabbitmq_orders_log/rabbitmq_orders_log.js
+++ metactical/metactical/doctype/rabbitmq_orders_log/rabbitmq_orders_log.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('RabbitMQ Orders Log', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.add_custom_button(__('Re-Sync Order'), function() {
+			frappe.call({
+				method: 'metactical.metactical.doctype.rabbitmq_orders_log.rabbitmq_orders_log.re_sync_order',
+				args: {
+					order_id: frm.doc.name
+				},
+				callback: function(r) {
+					if (r.message) {
+						frappe.show_alert(__('Order re-synced successfully.'));
+					} else {
+						frappe.show_alert(__('Failed to re-sync order.'));
+					}
+				}
+			});
+		})
 
-	// }
+
+		if (frm.doc.sales_order) {
+			frm.add_custom_button(__('View Sales Order'), function() {
+				frappe.set_route('Form', 'Sales Order', frm.doc.sales_order);
+			});
+		}
+	}
 });
