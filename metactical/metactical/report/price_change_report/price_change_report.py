4b0aa2dfe61da177e89b0daa1054d4f420542a5f
Aisenyi <malisa.aisenyi@gmail.com>
2025-08-14 00:28:34 +0300

    fix: Fixed cost in Price Change Report to get the item price for the default supplier

--- metactical/metactical/report/price_change_report/price_change_report.py
+++ metactical/metactical/report/price_change_report/price_change_report.py
@@ -84,7 +84,7 @@ def get_data(filters):
 	versions = frappe.db.sql("""
 				SELECT
 					item.item_code, item.ifw_retailskusuffix AS retail_sku, item.item_name, 
-					MAX(item_barcode.barcode) AS barcode, item.last_purchase_rate AS cost,
+					MAX(item_barcode.barcode) AS barcode,
 					CAST(version.creation AS DATE) AS date, user.full_name AS user_name,
 					version.data, item_price.price_list
 				FROM
@@ -100,7 +100,7 @@ def get_data(filters):
 				WHERE
 					CAST(version.creation AS DATE) = %(date)s AND ref_doctype = 'Item Price'
 				GROUP BY
-					retail_sku, item_name, cost, user_name, data
+					retail_sku, item_name, user_name, data
 				""", {"date": date}, as_dict=1)
 				
 	for version in versions:
@@ -112,5 +112,39 @@ def get_data(filters):
 				version["old_price"] = item[1]
 				version["new_price"] = item[2]
 		if price_changed:
+			version["cost"] = get_cost(version.item_code)
 			data.append(version)
 	return data
+
+def get_cost(item_code):
+	# Get the default supplier from Item Defaults
+	default_supplier = frappe.db.get_value(
+		"Item Default", 
+		{"parent": item_code, "default_supplier": ["!=", ""]}, 
+		"default_supplier"
+	)
+	
+	if not default_supplier:
+		return 0
+	
+	supplier_price_list = frappe.db.get_value(
+		"Supplier", 
+		default_supplier, 
+		"default_price_list"
+	)
+	
+	if not supplier_price_list:
+		return 0
+		
+	# Get the price from the supplier's price list for this item
+	price = frappe.db.get_value(
+		"Item Price", 
+		{
+			"item_code": item_code, 
+			"price_list": supplier_price_list,
+			"buying": 1
+		}, 
+		"price_list_rate"
+	)
+	
+	return float(price) if price else 0
