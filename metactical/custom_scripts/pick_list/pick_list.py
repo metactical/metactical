db13b3053936866aa3ae0574c6ef82228dceb232
nebiye lioul <lioulnebiye@gmail.com>
2025-08-18 21:14:49 +0300

    feat: Update item reordering logic in CustomPickList before_save and before_submit methods

--- metactical/custom_scripts/pick_list/pick_list.py
+++ metactical/custom_scripts/pick_list/pick_list.py
@@ -72,6 +72,8 @@ class CustomPickList(PickList):
 
 	def before_save(self):
 		super(CustomPickList, self).before_save()
+		self.reorder_items_by_location()
+
 		# Metactical Customization: removed auto location assignement. Will remove the whole
 		# set_item_location in the future from this page
 		# self.set_item_locations()
@@ -366,7 +368,7 @@ class CustomPickList(PickList):
 		
 	def before_submit(self):
 		super(CustomPickList, self).before_submit()
-		self.reorder_items_by_location()
+		# self.reorder_items_by_location()
 
 	def reorder_items_by_location(self):
 		# Sort items based on their location
