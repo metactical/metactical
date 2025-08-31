b65f62977166ca26e378873b140cde10967699cf
nebiye lioul <lioulnebiye@gmail.com>
2025-08-31 18:27:49 +0300

    feat: Implement email campaign functionality to send emails to leads

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -52,7 +52,8 @@ doctype_js = {
 	"Item": "custom_scripts/item/item.js",
 	"POS Profile": "custom_scripts/pos_profile/pos_profile.js",
  	"Item Group": "custom_scripts/item_group/item_group.js",
-	"Packing Slip": "custom_scripts/packing_slip/packing_slip.js"
+	"Packing Slip": "custom_scripts/packing_slip/packing_slip.js",
+	"Email Campaign": "custom_scripts/email_campaign/email_campaign.js"
 }
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 #doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
