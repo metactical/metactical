5ae1a794e0114a28333d4fb7d587c620c8164a7c
nebiye lioul <lioulnebiye@gmail.com>
2025-09-01 16:44:05 +0300

    feat: Add delay between email batches to manage sending rate

--- metactical/custom_scripts/email_campaign/email_campaign.py
+++ metactical/custom_scripts/email_campaign/email_campaign.py
@@ -4,7 +4,7 @@ from frappe import _
 from frappe.core.doctype.communication.email import make
 from frappe.model.document import Document
 from frappe.utils import add_days, getdate, today
-
+import time, sys
 
 # called through hooks to send campaign mails to leads
 def send_email_to_leads_or_contacts():
@@ -79,6 +79,9 @@ def send_mail(entry, email_campaign):
                     email_template=email_template,
                     sender=sender,
                 )
+            
+            sys.stdout.flush()
+            time.sleep(30)
 
             # Move to next chunk
             start += fetch_limit
