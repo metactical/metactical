c7b6faa9acaaabd1c3830f6a248e0b8d5358ee92
nebiye lioul <lioulnebiye@gmail.com>
2025-09-03 22:43:49 +0300

    feat: Add new DocTypes and scripts for Item Groups List and Paki Settings
    - Introduced Item Groups List with fields for item groups.
    - Created Paki Settings with multi-select options for item groups in reports.
    - Updated hooks.py to include new JavaScript files for the added DocTypes.
    - Added initial test cases for Paki Settings.

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -52,7 +52,9 @@ doctype_js = {
 	"Item": "custom_scripts/item/item.js",
 	"POS Profile": "custom_scripts/pos_profile/pos_profile.js",
  	"Item Group": "custom_scripts/item_group/item_group.js",
-	"Packing Slip": "custom_scripts/packing_slip/packing_slip.js"
+	"Packing Slip": "custom_scripts/packing_slip/packing_slip.js",
+	"BOM" : "custom_scripts/bom/bom.js",
+	"Operation": "custom_scripts/bom_operation/bom_operation.js"
 }
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 #doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
