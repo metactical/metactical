7f2db00c597d174913f562996fa64719f812cef7
nebiye lioul <lioulnebiye@gmail.com>
2025-10-09 14:49:44 +0300

    feat: Update item retrieval logic to filter by supplier price list in buying conditions

--- metactical/metactical/report/dead_stock_report/dead_stock_report.py
+++ metactical/metactical/report/dead_stock_report/dead_stock_report.py
@@ -426,7 +426,7 @@ def get_open_po_qty(item,supplier):
 def get_item_details(item, price_list, list_type="Selling",  supplier=None):
 	cond = " and selling = 1"
 	
-	if list_type == "Buying": cond= " and buying = 1"
+	if list_type == "Buying": cond= " and buying = 1 and price_list like 'SUP%'"
 	rate = 0
 	date = frappe.utils.nowdate()
 	r = frappe.db.sql("""select price_list_rate from `tabItem Price` where '{}' between valid_from and valid_upto and item_code = '{}'
@@ -454,7 +454,7 @@ def get_item_details(item, price_list, list_type="Selling",  supplier=None):
 @frappe.whitelist()
 def get_cost_details(item, list_type="Buying",  supplier=None):
 	
-	if list_type == "Buying": cond= " and buying = 1"
+	if list_type == "Buying": cond= " and buying = 1 and price_list like 'SUP%'"
 	rate = 0
 	date = frappe.utils.nowdate()
 	r = frappe.db.sql("select price_list_rate from `tabItem Price` where '{}' between valid_from and valid_upto and item_code = '{}' {} limit 1".format(date, item, cond))
