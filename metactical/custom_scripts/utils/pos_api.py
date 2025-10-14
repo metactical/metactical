2bfcf239855a41cd364106a646d3747046040a50
nebiye lioul <lioulnebiye@gmail.com>
2025-10-14 16:18:22 +0300

    Implement code changes to enhance functionality and improve performance

--- metactical/custom_scripts/utils/pos_api.py
+++ metactical/custom_scripts/utils/pos_api.py
@@ -537,7 +537,6 @@ def get_items(form_data):
         rate = item['Rate']
         qty = item['Qty']
         item_name = item['ItemName'] if 'ItemName' in item else ''
-        sales_person = frappe.db.get_value("User", {"full_name": item['SalesPerson']}, "name") if item['SalesPerson'] != "defaultSalesPersonId" else item['SalesPerson']
         
         item_info = {
             'item_code': item_code,
@@ -547,7 +546,6 @@ def get_items(form_data):
             'discount_percentage': item['Discount'],
             'warehouse': item["Warehouse"] if "Warehouse" in item else warehouse,
             'restock_fee': item['RestockFee'] if 'RestockFee' in item else 0.0,
-            'sales_person': sales_person,
         }
 
         if item_code == "2":
