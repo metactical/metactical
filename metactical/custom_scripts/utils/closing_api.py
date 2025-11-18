4bb747a5c15621eda55b3c5cbdfed4c6ca49611a
nebiye lioul <lioulnebiye@gmail.com>
2025-11-18 22:39:03 +0300

    feat: Submit End of Day Closing document after creation

--- metactical/custom_scripts/utils/closing_api.py
+++ metactical/custom_scripts/utils/closing_api.py
@@ -164,6 +164,7 @@ def create_closing_entry(*args, **kwargs):
         
         # Save the document
         end_of_day_closing.insert()
+        end_of_day_closing.submit()
         frappe.db.commit()
         
         frappe.response["status"] = "success"
