d378295db83be9ed09d431e257730b33c2678b30
nebiye lioul <lioulnebiye@gmail.com>
2025-11-25 16:26:50 +0300

    feat: Enhance validation and update methods to include parent class functionality

--- metactical/custom_scripts/item/item.py
+++ metactical/custom_scripts/item/item.py
@@ -41,12 +41,15 @@ class CustomItem(Item):
         new_item.save()
 
     def validate(self):
+        super().validate()
+        frappe.flags.in_import = False
         load_tags(self)
 
         if not self.description or self.description.strip() == '<div class="ql-editor read-mode"><p><br></p></div>':
             self.description = self.item_name
 
     def on_update(self):
+        super().on_update()
         # check website specification values
         validate_website_specifications(self)
         sync_website_specifications(self)
