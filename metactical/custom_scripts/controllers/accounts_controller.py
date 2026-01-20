d450e6de21283a60085af96f9382d9926efadaa8
nebiye lioul <lioulnebiye@gmail.com>
2026-01-20 14:48:48 +0300

    feat: add company linked addresses validation and enhance payment entry mode retrieval logic

--- metactical/custom_scripts/controllers/accounts_controller.py
+++ metactical/custom_scripts/controllers/accounts_controller.py
@@ -37,6 +37,9 @@ def validate_party_address(self, party, party_type, billing_address, shipping_ad
 		elif shipping_address and shipping_address not in party_address:
 			frappe.throw(_("Shipping Address does not belong to the {0}").format(party))
 
+def validate_company_linked_addresses(self):
+    return
+
 def set_order_defaults(
 	parent_doctype, parent_doctype_name, child_doctype, child_docname, trans_item
 ):
