59858023b38dd55440a6ae8571f47394b0a96669
nebiye lioul <lioulnebiye@gmail.com>
2026-01-20 11:24:02 +0300

    feat: implement address validation in sales order and enhance shipping address retrieval

--- metactical/__init__.py
+++ metactical/__init__.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 from frappe.utils.user import is_website_user
 import frappe
+from erpnext.controllers.accounts_controller import AccountsController
+from metactical.custom_scripts.controllers.accounts_controller import validate_party_address
 
 __version__ = '15.0.0'
+
+AccountsController.validate_party_address = validate_party_address
 
 def check_app_permission():
 	if frappe.session.user == "Administrator":
