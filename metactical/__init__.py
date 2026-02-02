0ea7cee08f2bf4c3b6d3d034af627a589b38400a
nebiye lioul <lioulnebiye@gmail.com>
2026-02-02 15:12:49 +0300

    refactor: comment out unused imports and duplicate address validation assignment

--- metactical/__init__.py
+++ metactical/__init__.py
@@ -2,17 +2,17 @@
 from __future__ import unicode_literals
 from frappe.utils.user import is_website_user
 import frappe
-from erpnext.controllers.accounts_controller import AccountsController
-from metactical.custom_scripts.controllers.accounts_controller import validate_party_address
+# from erpnext.controllers.accounts_controller import AccountsController
+# from metactical.custom_scripts.controllers.accounts_controller import validate_party_address
 
 __version__ = '0.0.1'
 
 #set_total_amount_to_default_mop = custom_default_mop
 #erpnext.controllers.taxes_and_totals.calculate_taxes_and_totals.set_total_amount_to_default_mop = custom_calculate_taxes_and_totals.set_total_amount_to_default_mop
 
-AccountsController.validate_party_address = validate_party_address
+# AccountsController.validate_party_address = validate_party_address
 
-AccountsController.validate_party_address = validate_party_address
+# AccountsController.validate_party_address = validate_party_address
 
 def check_app_permission():
 	if frappe.session.user == "Administrator":
