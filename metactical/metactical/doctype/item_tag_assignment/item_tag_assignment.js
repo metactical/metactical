07ccdc2e1cd829578ba6feea0a367d78a84627bc
nebiye lioul <lioulnebiye@gmail.com>
2026-02-02 22:04:04 +0300

    feat: enhance Item Tag Assignment with filter validation and UI updates

--- metactical/metactical/doctype/item_tag_assignment/item_tag_assignment.js
+++ metactical/metactical/doctype/item_tag_assignment/item_tag_assignment.js
@@ -1,12 +1,28 @@
 // Copyright (c) 2026, Storebuilder Commerce Inc and contributors
 // For license information, please see license.txt
+var filter_group = null;
 
 frappe.ui.form.on("Item Tag Assignment", {
-	refresh(frm) {
-
-	},
-    setup: function(frm) {
-		frm.get_field("conditions").$wrapper.html(
+	refresh(frm) {   
+        var filters = JSON.parse(frm.doc.filters || "[]");
+    
+        filters && frappe.model.with_doctype("Item", () => {
+            // Clear existing filters before adding new ones
+            filter_group.clear_filters();
+            filter_group.add_filters_to_filter_group(filters);
+        });
+    
+        frappe.db.count("Item", {
+            filters: filters
+        }).then((total) => {
+            frm.get_field("filters_detail").$wrapper.html(
+                `<p><b>${__("Total Items:")}  ${total}</b></p>`
+            );
+        });
+    },
+    
+	setup: function(frm) {
+		frm.get_field("filters_detail").$wrapper.html(
 			`<p><b>${__("Total Items:")}  0</b></p>`
 		);
 
@@ -14,9 +30,10 @@ frappe.ui.form.on("Item Tag Assignment", {
 			filter_group = new frappe.ui.FilterGroup({
 				parent: frm.get_field("conditions").$wrapper,
 				doctype: "Item",
+                default_filters: '[["Item","name","=","2"],["Item","ifw_discontinued","=",1]]',
 				on_change: () => {
 					if (!filter_group.get_filters().length) {
-						frm.get_field("conditions").$wrapper.html(
+						frm.get_field("filters_detail").$wrapper.html(
 							`<p><b>${__("Total Items:")}  0</b></p>`
 						);
 					}
@@ -26,11 +43,13 @@ frappe.ui.form.on("Item Tag Assignment", {
 						})
 						
 						total.then((total) => {
-							frm.get_field("conditions").$wrapper.html(
+							frm.get_field("filters_detail").$wrapper.html(
 								`<p><b>${__("Total Items:")}  ${total}</b></p>`
 							);
 						});
 					}
+
+                    frm.set_value("filters", JSON.stringify(frm.events.get_filters(filter_group)));
 				},
 			});
 		});
