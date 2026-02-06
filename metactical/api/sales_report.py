d5bc720383b2f538bd8979e17015e9782ec333c4
nebiye lioul <lioulnebiye@gmail.com>
2026-02-06 15:11:04 +0300

    separate sales report v9 for ASK and ICL

--- metactical/api/sales_report.py
+++ metactical/api/sales_report.py
@@ -26,3 +26,34 @@ def get_us_data(**args):
 		for item in items:
 			data[item.item_code] = item.qty
 	return data
+
+@frappe.whitelist(allow_guest=1)
+def get_us_sales_data(filters={}):
+    data = {}
+
+    conditions = ["bin.warehouse = %(warehouse)s"]
+    params = {"warehouse": "US01-Houston-Active - AOI"}
+    left_join = ""
+
+    if filters.get("supplier"):
+        left_join = " LEFT JOIN `tabItem Supplier` AS supplier ON supplier.parent = item.name"
+        conditions.append("supplier.supplier IN %(supplier)s")
+
+        # ensure tuple
+        params["supplier"] = tuple(filters.get("supplier"))
+
+    items = frappe.db.sql(f"""
+        SELECT
+            item.item_code,
+            actual_qty - reserved_qty AS qty
+        FROM `tabBin` AS bin
+        LEFT JOIN `tabItem` AS item
+            ON item.name = bin.item_code
+        {left_join}
+        WHERE {" AND ".join(conditions)}
+    """, params, as_dict=1)
+
+    for item in items:
+        data[item.item_code] = item.qty
+
+    return data
