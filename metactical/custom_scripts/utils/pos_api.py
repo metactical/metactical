cddc8fc43b8d548fd12111767877aad3ff274844
nebiye lioul <lioulnebiye@gmail.com>
2026-02-27 18:11:32 +0300

    fix: add warehouse information to items in update_sales_order function

--- metactical/custom_scripts/utils/pos_api.py
+++ metactical/custom_scripts/utils/pos_api.py
@@ -634,6 +634,7 @@ def update_sales_order(sales_order, form_data):
                 if (item['item_code'] == "2" and sales_item.item_name == item['item_name']) or (item['item_code'] == sales_item.item_code and item["item_code"] != "2"):
                     item["name"] = sales_item.name
                     item["docname"] = sales_item.name
+                    item["warehouse"] = sales_item.warehouse
                     item["conversion_factor"] = sales_item.conversion_factor
                     item["uom"] = sales_item.uom
                     item["idx"] = i + 1
