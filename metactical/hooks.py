ae89ff0726668d69a9b4f36d172fd5847c886c09
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2026-03-21 21:30:09 +0300

    Add custom shipping and billing address fields to Supplier fixtures

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -888,7 +888,9 @@ fixtures = [{
 			"Supplier-column_break_ov8xt",
 			"Item-month_on_hand",
 			"Item Group-category_names",
-			"Item Group-categoy_names_updated"
+			"Item Group-categoy_names_updated",
+   			"Supplier-custom_shipping_address",
+			"Supplier-custom_billing_address"
 		]]]
 	},
 	{
