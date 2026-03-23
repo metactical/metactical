790373b1c3e6e8b58106909a1a7518050bcff6c5
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2026-03-23 18:38:43 +0300

    feat: update supplier fixtures to include new custom address fields

--- metactical/hooks.py
+++ metactical/hooks.py
@@ -889,8 +889,10 @@ fixtures = [{
 			"Item-month_on_hand",
 			"Item Group-category_names",
 			"Item Group-categoy_names_updated",
-   			"Supplier-custom_shipping_address",
-			"Supplier-custom_billing_address"
+   			"Supplier-custom_billing_address",
+			"Supplier-custom_column_break_bm4eg",
+			"Supplier-custom_shipping_address",
+   			"Supplier-custom_default_address"
 		]]]
 	},
 	{
