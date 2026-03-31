ece313ddd3781fe1ebe2b3e79e7dde3df02a9804
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2026-03-31 21:27:41 +0300

    feat: enhance Rocket Chat message posting with improved error handling and dynamic message formatting

--- metactical/custom_scripts/utils/metactical_utils.py
+++ metactical/custom_scripts/utils/metactical_utils.py
@@ -52,14 +52,11 @@ def post_to_rocket_chat(doc, msg, failed=False, rmq=False, pos=False, attachment
 					frappe.log_error(title='Rocket Chat Error', message="PR Room ID not found in settings")
 					return
 
-		channel_name = rocket_chat_settings.channel_name if not pos else rocket_chat_settings.pos_failed_invoices
-   
-		headers = {
-			'X-Auth-Token': rocket_chat_settings.auth_token,
-			'X-User-Id': rocket_chat_settings.user_id
-		}
-	
 		if attachment and filename:
+			headers = {
+				'X-Auth-Token': rocket_chat_settings.auth_token,
+				'X-User-Id': rocket_chat_settings.user_id
+			}
 			base_url = rocket_chat_settings.url
 			upload_url = f"{base_url}/api/v1/rooms.upload/{rocket_chat_settings.pr_room_id}"
 
@@ -72,22 +69,45 @@ def post_to_rocket_chat(doc, msg, failed=False, rmq=False, pos=False, attachment
 				headers=headers,
 				files=files
 			)
-			if response.status_code != 200:
-				frappe.log_error(title='Rocket Chat Error', message=response.text)
+			if response.status_code == 200:
+				pass
+			else:
+				frappe.log_error(title='Rocket Chat Error', message=response.json())
 		else:
-			headers['Content-type'] = rocket_chat_settings.content_type or 'application/json'
-			payload = {
-				'channel': "#" + channel_name,
-				'text': msg
+			channel_name = rocket_chat_settings.channel_name if not pos else rocket_chat_settings.pos_failed_invoices
+      
+			headers = {
+				'Content-type': rocket_chat_settings.content_type or 'application/json',
+				'X-Auth-Token': rocket_chat_settings.auth_token,
+				'X-User-Id': rocket_chat_settings.user_id
 			}
-			response = requests.post(
-				rocket_chat_settings.url,
-				headers=headers,
-				data=json.dumps(payload)
-			)
 
-		if response.status_code != 200:
-			frappe.log_error(title='Rocket Chat Error', message=response.text)
+			if pos:	
+				message = msg
+			elif rmq:
+				message = msg
+			else:	
+				url = "/app/{0}/{1}".format(doc.doctype.lower().replace(" ", "-"), doc.name)
+				message = 'A document you submitted has taken too long and has been unquequd. Please resubmit the document and notify the system \
+								administrator \n[{0}]({1})'.format(get_url(url), get_url(url))
+				
+				if failed:
+					message = 'A document you submitted has failed. Please see the error in the comment section of the document and fix it \
+								\n[{0}]({1})'.format(get_url(url), get_url(url))
+
+			payload = {
+				'channel': "#"+channel_name,
+				'text': message
+			}
+
+			response = requests.post(rocket_chat_settings.url, 
+									headers=headers, 
+									data=json.dumps(payload))
+
+			if response.status_code == 200:
+				pass
+			else:
+				frappe.log_error(title='Rocket Chat Error', message=response.json())
 
 	except Exception as e:
 		frappe.log_error(title='Rocket Chat Error', message=frappe.get_traceback())
