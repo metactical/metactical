ea0eec53660bb3611e0b35e3fc767ac48c54fb9b
nebiye lioul <lioulnebiye@gmail.com>
2026-05-26 11:05:03 +0300

    feat: implement test cases and packing slip generation with validation

---
+++ metactical/metactical/doctype/pps_api_log/pps_api_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Storebuilder Commerce Inc and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PPS API Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
