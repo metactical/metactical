5d7a3b7e515cfa89bb508a839e2c34894ed55902
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2026-06-26 16:27:27 +0300

    feat: refactor Restock Notification logic and enhance data handling in related scripts

--- metactical/custom_scripts/utils/restock_notification_api.py
+++ metactical/custom_scripts/utils/restock_notification_api.py
@@ -1,26 +1,35 @@
 import frappe
+from frappe.utils import now_datetime
+
 
 def process_rmq_data(parsedContent):
 	try:
 		create_rmq_log(parsedContent)
-		
+
 	except Exception as e:
 		frappe.log_error(title='Restock Subscription Log', message=frappe.get_traceback())
-  
+
+
 def create_rmq_log(parsedContent):
 	try:
-		publisher_site = parsedContent.get("publisher_site", "Unknown")	
 		rmq_log = frappe.get_doc({
 			"doctype": "Restock Subscription Log",
+			"customer_name": parsedContent.get("CustomerName"),
+			"customer_email": parsedContent.get("Email"),
+			"retail_sku": parsedContent.get("RetailSkuSuffix"),
+			"lead_source": parsedContent.get("publisher_site", "Unknown"),
+			"item_price": parsedContent.get("ItemPrice"),
+			"received_at": now_datetime(),
 			"payload": as_unicode(parsedContent),
-			"lead_source": publisher_site
 		})
-  
-		rmq_log.insert()		
+
+		rmq_log.insert()
+		rmq_log.submit()
 		frappe.db.commit()
-  
+
 	except Exception as e:
 		frappe.log_error(title='Restock Subscription Log Creation Error', message=frappe.get_traceback())
+
 
 def as_unicode(text: str, encoding: str = "utf-8") -> str:
 	"""Convert to unicode if required"""
