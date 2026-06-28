574cd4e9834dc43afe33ea18bc266769929ef5ce
nebiye lioul <lioulnebiye@gmail.com>
2026-06-28 19:11:36 +0300

    feat: support multiple coupon code payments

--- metactical/custom_scripts/sales_order/sales_order.py
+++ metactical/custom_scripts/sales_order/sales_order.py
@@ -80,6 +80,13 @@ class SalesOrderCustom(SalesOrder):
 		# Metactical Customization: Added
 		for item in self.items:
 			frappe.enqueue(update_item_inventory_output, item_code=item.item_code, voucher_type="Sales Order", queue='default')
+   
+		coupon_codes = frappe.db.get_values("Coupon Code", {"neb_redeemed_in": self.name, "used": 0}, "name")
+		if coupon_codes:
+			for coupon_code in coupon_codes:
+				frappe.db.set_value("Coupon Code", coupon_code[0], {
+					"used": 1
+				})
 
 	def on_cancel(self):
 		super(SalesOrderCustom, self).on_cancel()
@@ -87,7 +94,21 @@ class SalesOrderCustom(SalesOrder):
 		# Metactical Customization: Added
 		for item in self.items:
 			update_item_inventory_output(item_code=item.item_code, voucher_type="Sales Order")
-
+			self.clean_up_coupon_codes()
+	
+	def on_trash(self):
+		super(SalesOrderCustom, self).on_trash()
+		self.clean_up_coupon_codes()
+	
+	def clean_up_coupon_codes(self):
+		coupon_codes = frappe.db.get_values("Coupon Code", {"neb_redeemed_in": self.name}, "name")
+		if coupon_codes:
+			for coupon_code in coupon_codes:
+				frappe.db.set_value("Coupon Code", coupon_code[0], {
+					"used": 0, 
+					"neb_redeemed_in": None
+				})
+	
 	def on_update_after_submit(self):
 		super().on_update_after_submit()
 
