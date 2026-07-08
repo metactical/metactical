6cf598ab3a361cabd800c9ff152fcd4c1d1c0735
Natnael Getnet <NatnaelGetnetHaregewoyn@gmail.com>
2026-07-08 16:28:16 +0300

    refactor: remove on_update method and associated item unlinking logic from SBTag

--- metactical/metactical/doctype/sb_tag/sb_tag.py
+++ metactical/metactical/doctype/sb_tag/sb_tag.py
@@ -5,21 +5,6 @@ import frappe
 from frappe.model.document import Document
 
 class SBTag(Document):
-	def on_update(self):
-		# When an SB Tag is disabled, unlink it from every item that references it.
-		if self.disabled:
-			count = frappe.db.count("Item SB Tag", {"sb_tag": self.name})
-			if count:
-				frappe.enqueue(
-					remove_sb_tag_from_items,
-					enqueue_after_commit=True,
-					sb_tag=self.name,
-				)
-				frappe.msgprint(
-					frappe._("Removing this SB Tag from {0} item(s) in the background.").format(count),
-					alert=True,
-				)
-
 	def after_rename(self, old_name, new_name, merge=False):
 		merge_history = frappe.get_doc({
 			'doctype': 'SB Tag Merge History',
@@ -41,8 +26,3 @@ class SBTag(Document):
 		merge_history.insert(ignore_permissions=True)
 		frappe.db.commit()
 
-
-def remove_sb_tag_from_items(sb_tag):
-	# Unlink a (now disabled) SB Tag from every item that references it.
-	frappe.db.delete("Item SB Tag", {"sb_tag": sb_tag})
-	frappe.db.commit()
