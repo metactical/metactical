ba116542c1982d64d156a1f5d80cdfa4a1444a33
Your Name <you@example.com>
2026-08-24 05:51:55 +0000

    feat: Added Procurement Workflow with Purchase Order V3 and related documents
    - Created Supplier Claim V3 document with fields for purchase order, supplier, claim date, and items.
    - Implemented Supplier Claim V3 Item document to manage individual claim items.
    - Added Supplier Order Confirmation V3 and its associated documents for managing supplier confirmations.
    - Introduced Supplier Order Confirmation V3 Backorder to track backordered items.
    - Developed Supplier Order Confirmation V3 Document for handling supplier documents related to orders.
    - Established necessary JavaScript and Python files for each document type to support functionality.

---
+++ metactical/metactical/doctype/supplier_claim_v3_item/supplier_claim_v3_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Storebuilder Commerce Inc and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SupplierClaimV3Item(Document):
+	pass
